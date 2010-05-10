4aa76a90ea19ca0ef4693edf71661df9c6900bcb
atorjusen <atorjusen@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2010-05-10 12:28:36 +0000

    Parsing abssamples as a Junit test
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@4377 97f52227-7fa9-4375-a72d-c1c733b1f48f

---
+++ frontend/tests/abs/frontend/parser/ParseSamplesTest.java
@@ -0,0 +1,63 @@
+package abs.frontend.parser;
+
+import static org.junit.Assert.fail;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.io.StringReader;
+
+import org.junit.Test;
+
+import abs.frontend.ast.Model;
+
+
+public class ParseSamplesTest {
+
+	@Test
+	public void testSamples() {
+		String dir="tests/abssamples/"; 	
+		String[] files = {"PeerToPeer.abs", "pingpong.abs"};
+		for (int i = 0 ; i < files.length; i++){
+			System.out.println("parsing " + files[i]);
+			assertParseOk(dir + files[i]);
+		}	
+	}
+	
+	protected static void assertParseOk(String s) {
+		Model m = null;
+		try {
+			m = parse(s);
+			if (m != null) {
+				int numSemErrs = m.errors().size();
+				if (numSemErrs > 0){
+					StringBuffer errs = new StringBuffer("Semantic errors: " + numSemErrs + "\n");
+					for (Object error : m.errors())
+						errs = errs.append(s + ":" + error + "\n");
+				fail(errs.toString());
+				}		
+			}	
+		}	catch (Throwable e) {
+			fail("Failed to parse: "+ s+"\n"+e.getMessage());
+		}
+	}
+
+	protected static Model parse(String file) throws Exception {
+		Reader reader = new FileReader(file);
+		ABSParser parser = new ABSParser();
+		ABSScanner scanner = new ABSScanner(reader);
+		Model m = (Model)parser.parse(scanner);
+		reader.close();
+		return m; 
+	}
+
+	
+	
+}
+	
+
+
+
+	
+
