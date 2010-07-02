bd64aa4cc0b704fad748fb205dd00d87335bdc55
jschaefer <jschaefer@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2010-07-02 08:15:28 +0000

    refactored ErrorCheck.jadd to use ErrorMessage enums
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@4928 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- frontend/tests/abs/frontend/analyser/ErrorMessage.java
+++ frontend/tests/abs/frontend/analyser/ErrorMessage.java
@@ -2,8 +2,12 @@ package abs.frontend.analyser;
 
 public enum ErrorMessage {
     CYCLIC_INHERITANCE("Cyclic inheritance chain for interface: %s"),
-    UNKOWN_INTERFACE("Unknown interface: %s");
-    
+    UNKOWN_INTERFACE("Unknown interface: %s"),
+    UNKOWN_DATATYPE("Unknown datatype: %s"),
+    UNKOWN_INTERFACE_OR_DATATYPE("Unknown interface or datatype: %s"),
+    DUPLICATE_TYPE_DECL("Duplicate type declaration: %s");
+
+        
     private String pattern;
 
     ErrorMessage(String pattern) {
