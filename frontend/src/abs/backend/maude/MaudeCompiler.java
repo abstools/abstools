76a365e04af6cbffa4d30b6ee26c334fc953f25c
rschlatte <rschlatte@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2010-08-18 13:42:46 +0000

    Restore Maude compilation
    ... keeping the shell script and including abslang.abs here for now
        until this is solved elsewhere.
    
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@5605 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- frontend/src/abs/backend/maude/MaudeCompiler.java
+++ frontend/src/abs/backend/maude/MaudeCompiler.java
@@ -5,6 +5,7 @@ import java.io.Reader;
 
 import abs.frontend.analyser.SemanticError;
 import abs.frontend.analyser.SemanticErrorList;
+import abs.frontend.ast.CompilationUnit;
 import abs.frontend.ast.Model;
 import abs.frontend.parser.ABSParser;
 import abs.frontend.parser.ABSScanner;
@@ -15,40 +16,45 @@ public class MaudeCompiler {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		Model m = null;
-        //if (args.length == 0) args = new String[]{"/Users/rudi/Source/hats/Tools/ABS/trunk/frontend/tests/abssamples/PeerToPeer.abs"};
+		Model m = new Model();
+		boolean hasErrors = false;
         for (String arg : args) {
             try {
-                m = parse(arg);
+            	m.addCompilationUnit(parse(arg));
             } catch (Error err) {
 				System.err.println("Parsing of " + arg + " failed with Error");
 				System.err.println(err);
 				err.printStackTrace(System.err);
-                return;
+                System.exit(1);
             } catch (Exception e) {
 				System.err.println("Parsing of " + arg +  " failed with Exception");
 				System.err.println(e);
 				e.printStackTrace(System.err);
-                return;
+                System.exit(1);
             }
-            SemanticErrorList l = m.typeCheck();
-            if (!l.isEmpty()) {
-                for (SemanticError e : l) {
-                	System.err.println(arg + ":" + e.getLine() + ": " + e.getMsg());
-                }
-            }
-            if (m != null && l.isEmpty()) m.generateMaude(System.out);
-            else System.exit(1);
         }
+        SemanticErrorList l = m.typeCheck();
+        if (!l.isEmpty()) {
+        	hasErrors = true;
+            for (SemanticError e : l) {
+            	System.err.println(e.getFileName() + ":" + e.getLine() + ": " + e.getMsg());
+            }
+        }
+        if (!hasErrors) {
+        	m.generateMaude(System.out);
+        	System.exit(0);
+        } else System.exit(1);
 	}
 
-	protected static Model parse(String file) throws Exception {
+	protected static CompilationUnit parse(String file) throws Exception {
 		Reader reader = new FileReader(file);
-		ABSParser parser = new ABSParser();
-		ABSScanner scanner = new ABSScanner(reader);
-		Model m = (Model)parser.parse(scanner);
-		reader.close();
-		return m;
+		try {
+			ABSParser parser = new ABSParser();
+			ABSScanner scanner = new ABSScanner(reader);
+			return parser.parse(file, scanner);
+		} finally {
+			reader.close();
+		}
 	}
 }
 
