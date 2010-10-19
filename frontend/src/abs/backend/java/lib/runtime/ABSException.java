70d36eb0251f1ff2da874c740182f06a579885b8
jschaefer <jschaefer@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2010-10-19 13:48:18 +0000

    print out random seed when exceptions are thrown
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@6219 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- frontend/src/abs/backend/java/lib/runtime/ABSException.java
+++ frontend/src/abs/backend/java/lib/runtime/ABSException.java
@@ -5,5 +5,12 @@ public class ABSException extends RuntimeException {
     public ABSException(String string) {
         super(string);
     }
+    
+    @Override
+    public String getMessage() {
+        String res = super.getMessage();
+        res += "\nRandom seed: "+Config.RANDOM_SEED;
+        return res;
+    }
 
 }
