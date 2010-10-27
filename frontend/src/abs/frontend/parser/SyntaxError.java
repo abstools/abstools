d6b17e9d9bbfef3e98cf9dca8a85b85c58c3cf8d
mweber <mweber@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2010-10-27 13:09:23 +0000

    Added markers for errors in abs-files. Added method to gain access to the token at which the SyntaxError occurred.
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@6254 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- frontend/src/abs/frontend/parser/SyntaxError.java
+++ frontend/src/abs/frontend/parser/SyntaxError.java
@@ -1,17 +1,23 @@
 package abs.frontend.parser;
 
+import beaver.Symbol;
 import abs.common.CompilerError;
 
 public class SyntaxError extends ParserError  {
 
 	private static final long serialVersionUID = 1L;
+	private Symbol token;
 
 	public SyntaxError(final String message) {
-		this(message,0,0);
+		this(message,0,0, null);
 	}
 
-	public SyntaxError(final String message, int lineNumber, int columnNumber) {
+	public SyntaxError(final String message, int lineNumber, int columnNumber, Symbol token) {
 		super(message,lineNumber,columnNumber);
+		this.token = token;
 	}
-
+	
+	public Symbol getToken(){
+		return token;
+	}
 }
