90041fe4a40087597a7fecfb4b7623f02b97263c
jschaefer <jschaefer@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2010-11-09 17:58:44 +0000

    use VarOrFieldUse type in AssignStmt
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@6377 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- frontend/src/abs/frontend/parser/Keywords.java
+++ frontend/src/abs/frontend/parser/Keywords.java
@@ -1,5 +1,12 @@
 package abs.frontend.parser;
 
+/**
+ * This class is currently used by the Eclipse-Plugin
+ * as we have not found yet a better way to get a list
+ * of all keywords. The list must be hold consistent
+ * with the list defined in the ABS.flex file manually
+ *
+ */
 public class Keywords {
 	
 	//This list has to be consistent with keywords in ABS.flex
