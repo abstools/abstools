5ad6ee1f261ada5dffaeeac1393f7911f91236fd
jschaefer <jschaefer@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2010-11-10 14:52:16 +0000

    improved error recovery in parser
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@6393 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- frontend/tests/abs/frontend/parser/RecoverTest.java
+++ frontend/tests/abs/frontend/parser/RecoverTest.java
@@ -7,23 +7,75 @@ import org.junit.Test;
 import abs.frontend.FrontendTest;
 import abs.frontend.ast.ClassDecl;
 import abs.frontend.ast.Decl;
+import abs.frontend.ast.MethodImpl;
 import abs.frontend.ast.Model;
 
 public class RecoverTest extends FrontendTest {
-    
 
     @Test
-    public void declTest() {
-        Model m = assertParseError(" class C { class D { }");
-        assertContainsDeclWithName(m,"D");
+    public void importTest() {
+        Model m = assertParseError(" x class C { }");
+        assertContainsDeclWithName(m,"C");
     }
 
     @Test
     public void intfTest() {
-        Model m = assertParseError(" class I { ");
+        Model m = assertParseError(" interface I { class C { }");
+        assertContainsDeclWithName(m,"I");
+    }
+
+    @Test
+    public void intfTest2() {
+        Model m = assertParseError(" interface I class C { }");
+        assertContainsDeclWithName(m,"I");
+    }
+
+    @Test
+    public void intfTest3() {
+        Model m = assertParseError(" interface I extends J,K class C { }");
+        assertContainsDeclWithName(m,"I");
+    }
+
+    @Test
+    public void intfTest4() {
+        Model m = assertParseError(" interface I { I m() } class C { }");
         assertContainsDeclWithName(m,"I");
     }
     
+    @Test
+    public void classDeclTest() {
+        Model m = assertParseError(" class C { class D { }");
+        assertContainsDeclWithName(m,"C");
+    }
+
+    @Test
+    public void classDeclTest2() {
+        Model m = assertParseError(" class C implements I { class D { }");
+        assertContainsDeclWithName(m,"C");
+    }
+    
+    @Test
+    public void classDeclTest3() {
+        Model m = assertParseError(" class C implements I { XXX asd statasd asdasd }");
+        assertContainsDeclWithName(m,"C");
+    }
+
+    @Test
+    public void methodTest3() {
+        Model m = assertParseError(" class C { Unit m() { asdasd } }");
+        assertContainsMethodWithName(m,"m");
+    }
+    
+    private void assertContainsMethodWithName(Model m, String name) {
+        boolean found = false;
+        for (MethodImpl mi : ((ClassDecl) m.getCompilationUnit(0).getModuleDecl(0).getDecl(0)).getMethods()) {
+            if (mi.getMethodSig().getName().equals(name)) {
+                found = true;
+            }
+        }
+        assertTrue("Did not found method with name "+name,found);
+    }
+
     private void assertContainsDeclWithName(Model m, String name) {
         boolean found = false;
         for (Decl d : m.getCompilationUnit(0).getModuleDecl(0).getDecls()) {
