8ee400dae34380dcbf219995cceced86dce174ee
ramus <ramus@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2011-02-27 20:20:19 +0000

    original() call now working (also in flatland).
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@9429 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- frontend/src/abs/frontend/delta/exceptions/ASTNodeNotFoundException.java
+++ frontend/src/abs/frontend/delta/exceptions/ASTNodeNotFoundException.java
@@ -3,15 +3,7 @@ package abs.frontend.delta.exceptions;
 import abs.frontend.ast.ASTNode;
 
 public class ASTNodeNotFoundException extends Exception {
-    String message;
-
     public ASTNodeNotFoundException(String msg) {
-        message = msg;
+        super(msg);
     }
-
-
-    public String toString() {
-        return "AST Node Not Found: " + message;
-    }
-
 }
