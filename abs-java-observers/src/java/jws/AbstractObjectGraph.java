f0ce32cb49ec12851378ad4376cb8e4f47e6ef5e
jschaefer <jschaefer@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2011-03-02 11:57:25 +0000

    implemented gml output
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@9543 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- abs-java-observers/src/java/jws/AbstractObjectGraph.java
+++ abs-java-observers/src/java/jws/AbstractObjectGraph.java
@@ -10,7 +10,9 @@ import abs.backend.java.observing.ObjectView;
 public abstract class AbstractObjectGraph implements ObjectGraph {
 
 	final Map<COGView, ObjectView> initialObjects = new HashMap<COGView, ObjectView>();
+	final Map<ObjectView, Integer> objectIDs = new HashMap<ObjectView, Integer>();
 	final AtomicInteger edgeCounter = new AtomicInteger();
+   final AtomicInteger nodeCounter = new AtomicInteger();
    
 	
 	
@@ -19,9 +21,18 @@ public abstract class AbstractObjectGraph implements ObjectGraph {
 	}
 
 	protected String getID(ObjectView o) {
-      return o.getClassName()+" "+o.getID();
+	   Integer id = objectIDs.get(o);
+	   if (id == null) {
+	      id = nodeCounter.incrementAndGet();
+	      objectIDs.put(o,id);
+	   }
+      return String.valueOf(id);
    }
 
+   protected String getLabel(ObjectView o) {
+      return o.getClassName()+" "+o.getID();
+   }
+	
 	@Override
    public void addObject(ObjectView o) {
    	addObject(o,false);
--- abs-java-observers/src/java/jws/GraphStreamGraph.java
+++ abs-java-observers/src/java/jws/GraphStreamGraph.java
@@ -1,7 +1,11 @@
 package jws;
 
 import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Random;
@@ -13,6 +17,7 @@ import org.graphstream.graph.Node;
 import org.graphstream.graph.implementations.MultiGraph;
 import org.graphstream.stream.file.FileSink;
 import org.graphstream.stream.file.FileSinkDGS;
+import org.graphstream.stream.file.FileSinkGML;
 import org.graphstream.ui.layout.Layout;
 import org.graphstream.ui.layout.Layouts;
 import org.graphstream.ui.swingViewer.GraphRenderer;
@@ -70,15 +75,17 @@ public class GraphStreamGraph extends AbstractObjectGraph {
 
    }
    
+   
+   
 	@Override
    public void addObject(final ObjectView o, final boolean COG) {
 		super.addObject(o, COG);
 		
 		Node n = graph.addNode(getID(o));
 		n.addAttribute("ui.label", getID(o));
-		n.addAttribute("Label", getID(o));
-		n.addAttribute("abs.cog", getID(getCOGOwner(o)));
-		n.addAttribute("abs.class", o.getClassName());
+		n.addAttribute("label", getLabel(o));
+		n.addAttribute("cog", getID(getCOGOwner(o)));
+		n.addAttribute("class", o.getClassName());
 		if (COG) {
 			n.addAttribute("layout.weight", 0.5f);
 			n.addAttribute("ui.class", "cog");
@@ -109,7 +116,7 @@ public class GraphStreamGraph extends AbstractObjectGraph {
 
 	@Override
    public void addEdge(ObjectView source, ObjectView target, boolean cog) {
-		Edge e = graph.addEdge("edge-"+edgeCounter.incrementAndGet(),getID(source),getID(target),true);
+		Edge e = graph.addEdge(String.valueOf(edgeCounter.incrementAndGet()),getID(source),getID(target),true);
 		if (cog) {
 			e.addAttribute("ui.class", "cog");
 			e.addAttribute("layout.weight", 0.1f);
@@ -140,18 +147,21 @@ public class GraphStreamGraph extends AbstractObjectGraph {
 				return viewer;
 	}
 
-	private String fileName;
+	private String dgsFileName;
+	private String filePrefix;
 	private FileSink fileSink;
 	
 
 	@Override
 	public void begin() {
+	   graph.display();
 		fileSink = new FileSinkDGS();
 		graph.addSink(fileSink);
 		Random r = new Random();
-      fileName = "graph"+r.nextInt(Integer.MAX_VALUE)+ ".dgs";
+		filePrefix = "graph"+r.nextInt(Integer.MAX_VALUE);
+      dgsFileName = filePrefix+ ".dgs";
       try {
-	      fileSink.begin(fileName);
+	      fileSink.begin(dgsFileName);
       } catch (IOException e) {
 	      // TODO Auto-generated catch block
 	      e.printStackTrace();
@@ -162,13 +172,62 @@ public class GraphStreamGraph extends AbstractObjectGraph {
    public void end() {
 		try {
 	      fileSink.end();
-	      File f = new File(fileName);
-	      System.out.println("Graph written to "+f.toURI().toURL().toExternalForm());
+	      File f = new File(dgsFileName);
+	      System.out.println("DGS Graph written to "+f.toURI().toURL().toExternalForm());
+	      MyGMLWriter mw = new MyGMLWriter();
+         File file = new File(filePrefix+".gml");
+	      mw.writeAll(graph, file);
+         System.out.println("GML Graph written to "+file.toURI().toURL().toExternalForm());
       } catch (IOException e) {
-	      // TODO Auto-generated catch block
 	      e.printStackTrace();
       }
    }
 }
 
+
+class MyGMLWriter {
+   private PrintWriter pw;
+
+   public synchronized void writeAll(Graph g, File file) throws FileNotFoundException {
+      pw = new PrintWriter(new FileOutputStream(file));
+      
+      pw.println("Creator "+escape("ABS Graph Writer"));
+      pw.println("graph [");
+      
+      for (Node n : g.getNodeSet()) {
+         writeNode(n);
+      }
+      
+      for (Edge e : g.getEdgeSet()) {
+         writeEdge(e);
+      }
+      
+      pw.println("]");
+      
+      pw.close();
+      pw = null;
+   }
+   
+   private void writeEdge(Edge e) {
+      pw.println("\t edge [");
+      pw.println("\t\t source "+e.getSourceNode().getId());
+      pw.println("\t\t target "+e.getTargetNode().getId());
+      pw.println("\t ]");
+      
+   }
+
+   private void writeNode(Node n) {
+      pw.println("\t node [");
+      pw.println("\t\t id "+n.getId());
+      pw.println("\t\t label "+escape((String)n.getAttribute("label")));
+      pw.println("\t\t kind "+escape("object"));
+      pw.println("\t\t class "+escape((String)n.getAttribute("class")));
+      pw.println("\t\t cog "+n.getAttribute("cog"));
+      pw.println("\t ]");
+   }
+
+   public static String escape(String s) {
+      return "\"" + s + "\"";
+   }
+}
 
