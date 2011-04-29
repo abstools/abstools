abstools/abstools

commit 77b5a3e1aaa30c14665c3b8a0b4dc352a3a31184

added support for abs packages
git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@10257 97f52227-7fa9-4375-a72d-c1c733b1f48f

eclipse-plugin/src/eu/hatsproject/absplugin/editor/contentassist/ProposalFactory.java
@@ -4,14 +4,9 @@
  */
 package eu.hatsproject.absplugin.editor.contentassist;
 
-import static abs.frontend.parser.Main.parseUnit;
 import static eu.hatsproject.absplugin.util.Images.NO_IMAGE;
 import static eu.hatsproject.absplugin.util.Images.getImageForASTNode;
-import static eu.hatsproject.absplugin.util.UtilityFunctions.getASTNodeOfOffset;
-import static eu.hatsproject.absplugin.util.UtilityFunctions.getAbsNature;
-import static eu.hatsproject.absplugin.util.UtilityFunctions.getSuperOfASTNode;
-import static eu.hatsproject.absplugin.util.UtilityFunctions.isABSFile;
-import static eu.hatsproject.absplugin.util.UtilityFunctions.standardExceptionHandling;
+import static eu.hatsproject.absplugin.util.UtilityFunctions.*;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -32,6 +27,7 @@ import org.eclipse.jface.text.contentassist.CompletionProposal;
 import org.eclipse.jface.text.contentassist.ICompletionProposal;
 
 import abs.frontend.ast.*;
+import abs.frontend.parser.Main;
 import abs.frontend.typechecker.KindedName;
 import abs.frontend.typechecker.KindedName.Kind;
 import abs.frontend.typechecker.ResolvedName;
@@ -61,6 +57,7 @@ public class ProposalFactory{
 		private List<ICompletionProposal> proposals;
 		private CompilationUnit cu;
 		private ABSEditor editor;
+		private Main absParser;
 
 		/**
 		 * Initializes the {@link ProposalFactory} by parsing all abs files in the current project and
@@ -78,6 +75,9 @@ public class ProposalFactory{
 			this.doc = doc;
 			this.proposals = proposals;
 			this.editor = editor;
+			absParser.setAllowIncompleteExpr(true);
+		        absParser.setTypeChecking(false);
+			
 			
 			parseProject();
 		}
@@ -87,12 +87,12 @@ public class ProposalFactory{
 				final IncrementalModelBuilder builder,
 				IResource resource) throws IOException,
 				CoreException, NoModelException {
-			if (isABSFile(resource)) {
+			if (isABSSourceFile(resource)) {
 				IFile visitedfile = (IFile) resource;
-				CompilationUnit cu = parseUnit(visitedfile.getLocation().toFile(),
-						null, new InputStreamReader(visitedfile.getContents()), true, true);
+				CompilationUnit cu = absParser.parseUnit(visitedfile.getLocation().toFile(),
+						null, new InputStreamReader(visitedfile.getContents()));
 				cu.setName(visitedfile.getLocation().toFile().getAbsolutePath());
-				builder.setCompilationUnit(cu);
+				builder.addCompilationUnit(cu);
 			}
 		}
 
@@ -120,10 +120,13 @@ public class ProposalFactory{
 					}
 				});
 					// compile the current document with non-saved changes
-					cu = parseUnit(file.getLocation().toFile(), null,
-							new StringReader(prepareDocContent()), true, true);
+				        Main absParser = new Main();
+			                absParser.setAllowIncompleteExpr(true);
+			                absParser.setTypeChecking(false);
+					cu = absParser.parseUnit(file.getLocation().toFile(), null,
+							new StringReader(prepareDocContent()));
 					cu.setName(file.getLocation().toFile().getAbsolutePath());
-					builder.setCompilationUnit(cu);
+					builder.addCompilationUnit(cu);
 			} catch (CoreException e) {
 				standardExceptionHandling(e);
 			} catch (NoModelException e) {

eclipse-plugin/build.xml
@@ -1,6 +1,6 @@
 <?xml version="1.0"?>
 <project name="ABS Update Site" default="compile" basedir=".">
-    <property name="revision.prefix" value="1.0.4" />
+    <property name="revision.prefix" value="1.0.5" />
 
 	<property name="update-site.dir" location="${basedir}/update-site" />
 	<property name="frontend.dir" location="../frontend" />

eclipse-plugin/src/eu/hatsproject/absplugin/builder/AbsNature.java
@@ -7,6 +7,8 @@ package eu.hatsproject.absplugin.builder;
 import static eu.hatsproject.absplugin.util.Constants.*;
 
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 
@@ -21,6 +23,7 @@ import abs.frontend.analyser.SemanticErrorList;
 import abs.frontend.ast.ASTNode;
 import abs.frontend.ast.CompilationUnit;
 import abs.frontend.ast.Model;
+import abs.frontend.parser.Main;
 import abs.frontend.parser.ParserError;
 import abs.frontend.parser.SyntaxError;
 import abs.frontend.typechecker.locationtypes.LocationType;
@@ -222,40 +225,28 @@ public class AbsNature implements IProjectNature {
 			   if (!file.isSynchronized(IResource.DEPTH_ZERO)) {
 			      file.refreshLocal(IResource.DEPTH_ZERO, null);
 			   }
-				CompilationUnit cu = abs.frontend.parser.Main.parseUnit(file.getLocation().toFile(), null, new InputStreamReader(file.getContents()), true, withincomplete);
-				cu.setName(file.getLocation().toFile().getAbsolutePath());
-				builder.setCompilationUnit(cu);
+			   
+			   Main m = new Main();
+			   m.setWithStdLib(true);
+			   m.setAllowIncompleteExpr(true);
+
+			   List<CompilationUnit> units = new ArrayList<CompilationUnit>(1);
+			   if (isABSPackage(file)) {
+			      units.addAll(m.parseABSPackageFile(file.getLocation().toFile()));
+			   } else {
+			      CompilationUnit cu = m.parseUnit(file.getLocation().toFile(), null, new InputStreamReader(file.getContents()));
+			      cu.setName(file.getLocation().toFile().getAbsolutePath());
+			      units.add(cu);
+			   }
+				builder.addCompilationUnits(units);
 				
-				if(cu.hasParserErrors()){
-					for(ParserError err : cu.getParserErrors()){
-
-						int startline   = err.getLine()-1;
-						int startcolumn = err.getColumn()-1;
-						int endline;
-						int endcolumn;
-
-						if(err instanceof SyntaxError){
-							SyntaxError serr = (SyntaxError)err;
-							int end   = serr.getToken().getEnd();
-							endline   = Symbol.getLine(end)-1;
-							endcolumn = Symbol.getColumn(end);
-						} else {
-							endcolumn = -1;
-							endline   = startline;
-						}
-
-						String message = err.getMessage();
-						int severity   = IMarker.SEVERITY_ERROR;
-						IMarker marker = file.createMarker(PARSE_MARKER_TYPE);
-						marker.setAttribute(IMarker.MESSAGE, message);
-						marker.setAttribute(IMarker.SEVERITY, severity);
-						marker.setAttribute(START_LINE, startline);
-						marker.setAttribute(START_COLUMN, startcolumn);
-						marker.setAttribute(END_LINE, endline);
-						marker.setAttribute(END_COLUMN, endcolumn);
-						marker.setAttribute(IMarker.LINE_NUMBER, startline+1);
-					}
-					return;
+				for (CompilationUnit cu : units) {
+				   if(cu.hasParserErrors()){
+				      for(ParserError err : cu.getParserErrors()){
+				         addMarker(file, err);
+				      }
+				      return;
+				   }
 				}
 			} catch(NoModelException e){
 				//ignore
@@ -264,6 +255,34 @@ public class AbsNature implements IProjectNature {
 			}
 		}
 	}
+
+   private static void addMarker(IFile file, ParserError err) throws CoreException {
+      int startline   = err.getLine()-1;
+      int startcolumn = err.getColumn()-1;
+      int endline;
+      int endcolumn;
+
+      if(err instanceof SyntaxError){
+      	SyntaxError serr = (SyntaxError)err;
+      	int end   = serr.getToken().getEnd();
+      	endline   = Symbol.getLine(end)-1;
+      	endcolumn = Symbol.getColumn(end);
+      } else {
+      	endcolumn = -1;
+      	endline   = startline;
+      }
+
+      String message = err.getMessage();
+      int severity   = IMarker.SEVERITY_ERROR;
+      IMarker marker = file.createMarker(PARSE_MARKER_TYPE);
+      marker.setAttribute(IMarker.MESSAGE, message);
+      marker.setAttribute(IMarker.SEVERITY, severity);
+      marker.setAttribute(START_LINE, startline);
+      marker.setAttribute(START_COLUMN, startcolumn);
+      marker.setAttribute(END_LINE, endline);
+      marker.setAttribute(END_COLUMN, endcolumn);
+      marker.setAttribute(IMarker.LINE_NUMBER, startline+1);
+   }
 	
 	/**
 	 * takes the properties from the project preference store for location type checking and location type precision. Typeckecks

eclipse-plugin/src/eu/hatsproject/absplugin/decorators/FolderDecorator.java
@@ -22,6 +22,7 @@ import org.eclipse.jface.viewers.LabelProviderChangedEvent;
 import org.eclipse.ui.PlatformUI;
 
 import eu.hatsproject.absplugin.util.Constants;
+import eu.hatsproject.absplugin.util.UtilityFunctions;
 
 /**
  * This Decorator is used for decorating folders with the ABS project icon. Only
@@ -42,11 +43,13 @@ public class FolderDecorator extends LabelProvider implements ILightweightLabelD
 			if (hasABSFiles((IFolder)element)){
 				decoration.addOverlay(MARKER_DESCRIPTOR);
 			}
-		}/*else if (element instanceof IFile){
-			if (checkFile((IResource)element)){
-				decoration.addOverlay(MARKER_DESCRIPTOR);				
-			}
-		}*/
+		} else if (element instanceof IFile){
+		   if (UtilityFunctions.isABSPackage((IFile)element)) {
+		      //if (checkFile((IResource)element)){
+		      decoration.addOverlay(MARKER_DESCRIPTOR);				
+		      //}
+		   }
+		}
 		
 	}
 	
@@ -77,7 +80,7 @@ public class FolderDecorator extends LabelProvider implements ILightweightLabelD
 	}
 
 	private boolean checkFile(IResource member) {
-		if (member instanceof IFile && member.getName().endsWith("." + Constants.ABS_FILE_EXTENSION)) {
+		if (UtilityFunctions.isABSSourceFile(member)) {
 			return true;
 		}
 		return false;

eclipse-plugin/src/eu/hatsproject/absplugin/editor/ABSCodeScanner.java
@@ -29,6 +29,7 @@ import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.widgets.Display;
 
 import abs.frontend.ast.*;
+import abs.frontend.parser.Main;
 import abs.frontend.parser.ParseException;
 import abs.frontend.typechecker.KindedName;
 import abs.frontend.typechecker.KindedName.Kind;
@@ -259,11 +260,12 @@ public class ABSCodeScanner implements ITokenScanner {
 		String doccontent = document.get();
 		try {
 			compilationUnit = null;
-			compilationUnit = abs.frontend.parser.Main.parseUnit(
+			Main absParser = new Main();
+			absParser.setTypeChecking(false);
+			compilationUnit = absParser.parseUnit(
 					new File(fEditor.getEditorInput().getName()),
 					doccontent,
-					new StringReader(doccontent),
-					true);
+					new StringReader(doccontent));
 		} catch (ParseException e) {
 			if (doDebug)
 				e.printStackTrace();

eclipse-plugin/src/eu/hatsproject/absplugin/internal/IncrementalModelBuilder.java
@@ -17,6 +17,7 @@ import abs.frontend.ast.ASTNode;
 import abs.frontend.ast.CompilationUnit;
 import abs.frontend.ast.List;
 import abs.frontend.ast.Model;
+import abs.frontend.parser.Main;
 import abs.frontend.typechecker.locationtypes.LocationType;
 import abs.frontend.typechecker.locationtypes.infer.LocationTypeInferrerExtension;
 import abs.frontend.typechecker.locationtypes.infer.LocationTypeInferrerExtension.LocationTypingPrecision;
@@ -38,19 +39,18 @@ public class IncrementalModelBuilder {
 		node.flushCache();
 	}
 	
-	public synchronized void setCompilationUnit(CompilationUnit cu) throws IOException, NoModelException  {
+    
+    public synchronized void addCompilationUnits(Iterable<CompilationUnit> units) throws IOException, NoModelException {
+       for (CompilationUnit u : units) {
+          addCompilationUnit(u);
+       }
+    }
+    
+	public synchronized void addCompilationUnit(CompilationUnit cu) throws IOException, NoModelException  {
 		if(model == null){
 			model = new Model();
 			
-			CompilationUnit stdLib = abs.frontend.parser.Main.getStdLib();
-			File bundle = FileLocator.getBundleFile(Platform.getBundle(ABSFRONTEND_PLUGIN_ID));
-
-			File src = new File(bundle, stdLib.getFileName());
-			if (!src.exists()) {
-				src = new File(bundle, "src/"+stdLib.getFileName());
-			}
-			stdLib.setName(src.getAbsolutePath());
-			model.addCompilationUnit(stdLib);
+			model.addCompilationUnit(getStdLibCompilationUnit());
 			model.addCompilationUnit(cu);
 			return;
 		}
@@ -69,6 +69,18 @@ public class IncrementalModelBuilder {
 //		model.flushCache();
 		flushAll(model);
 	}
+
+   private CompilationUnit getStdLibCompilationUnit() throws IOException {
+      CompilationUnit stdLib = new Main().getStdLib();
+      File bundle = FileLocator.getBundleFile(Platform.getBundle(ABSFRONTEND_PLUGIN_ID));
+
+      File src = new File(bundle, stdLib.getFileName());
+      if (!src.exists()) {
+      	src = new File(bundle, "src/"+stdLib.getFileName());
+      }
+      stdLib.setName(src.getAbsolutePath());
+      return stdLib;
+   }
 	
 	public synchronized void removeCompilationUnit(CompilationUnit cu) throws NoModelException{
 		if(model == null)

eclipse-plugin/src/eu/hatsproject/absplugin/util/UtilityFunctions.java
@@ -40,6 +40,7 @@ import abs.frontend.ast.CompilationUnit;
 import abs.frontend.ast.DataConstructorExp;
 import abs.frontend.ast.Model;
 import abs.frontend.ast.ModuleDecl;
+import abs.frontend.parser.ABSPackageFile;
 import abs.frontend.parser.SourcePosition;
 import beaver.Symbol;
 import eu.hatsproject.absplugin.Activator;
@@ -582,15 +583,35 @@ public class UtilityFunctions {
 		return false;
 	}
 	
+	public static boolean isABSSourceFile(IResource file){
+	    if (file != null && file instanceof IFile)
+	       return hasABSFileExtension((IFile)file);
+	    return false;
+	}
+
 	public static boolean isABSFile(IResource file){
 		if (file != null && file instanceof IFile){
-			return hasABSFileExtension((IFile)file);
+			return hasABSFileExtension((IFile)file) || isABSPackage((IFile)file);
 		}
 		
 		return false;
 	}	
 	
-	/**
+
+	// assumes file != null
+	public static boolean isABSPackage(IFile file) {
+      if (! "jar".equals(file.getFileExtension()))
+         return false;
+      
+      try {
+         return new ABSPackageFile(file.getLocation().toFile()).isABSPackage();
+      } catch (IOException e) {
+         e.printStackTrace();
+         return false;
+      }
+   }
+
+   /**
 	 * If plug-in in debug mode, then print exception stack trace.
 	 * @param e
 	 */

eclipse-plugin/tests/eu/hatsproject/absplugin/internal/ModelBuilderTest.java
@@ -16,6 +16,7 @@ import org.junit.Test;
 
 import abs.frontend.analyser.SemanticErrorList;
 import abs.frontend.ast.CompilationUnit;
+import abs.frontend.parser.Main;
 import abs.frontend.typechecker.locationtypes.infer.LocationTypeInferrerExtension.LocationTypingPrecision;
 import eu.hatsproject.absplugin.internal.IncrementalModelBuilder;
 import eu.hatsproject.absplugin.internal.NoModelException;
@@ -232,10 +233,11 @@ public class ModelBuilderTest {
 			"  node3!run();\n"+
 			"}\n";
 
-		CompilationUnit testcu = abs.frontend.parser.Main.parseUnit(new File("PeerToPeer.abs"), moduleText, new StringReader(moduleText), true);
+      Main absParser = new Main();
+		CompilationUnit testcu = absParser.parseUnit(new File("PeerToPeer.abs"), moduleText, new StringReader(moduleText));
 		assertEquals(0, testcu.getParserErrors().size());
 		try{
-			modelbuilder.setCompilationUnit(testcu);
+			modelbuilder.addCompilationUnit(testcu);
 			SemanticErrorList testel = modelbuilder.typeCheckModel(true, "Somewhere", LocationTypingPrecision.BASIC.toString());
 			assertEquals(2, testel.size()); //Why 2? Module not resolved gets inserted 2 times.
 		} catch(RuntimeException e){
@@ -246,9 +248,9 @@ public class ModelBuilderTest {
 			fail("Exception while typechecking!" + e.toString());
 		}
 		String importTestText = "module ImportTest;";
-		CompilationUnit importTestCU = abs.frontend.parser.Main.parseUnit(new File("importtest.abs"), importTestText, new StringReader(importTestText), true);
+		CompilationUnit importTestCU = absParser.parseUnit(new File("importtest.abs"), importTestText, new StringReader(importTestText));
 		try{
-			modelbuilder.setCompilationUnit(importTestCU);
+			modelbuilder.addCompilationUnit(importTestCU);
 			SemanticErrorList testel = modelbuilder.typeCheckModel(true, "Somewhere", LocationTypingPrecision.BASIC.toString());
 			assertEquals(0, testel.size());
 		} catch(RuntimeException e){

commit 8948033a5a69b0c5bc5d9244e0ff52cff8a5fdd2

Fix a bug in ProposalFactory introduced in r10257. This commit ensures private field absParser is properly initialised to avoid NullPointerException.
git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@10455 97f52227-7fa9-4375-a72d-c1c733b1f48f

eclipse-plugin/src/eu/hatsproject/absplugin/editor/contentassist/ProposalFactory.java
@@ -75,10 +75,9 @@ public class ProposalFactory{
 			this.doc = doc;
 			this.proposals = proposals;
 			this.editor = editor;
-			absParser.setAllowIncompleteExpr(true);
-		        absParser.setTypeChecking(false);
-			
-			
+			this.absParser = new Main();
+			this.absParser.setAllowIncompleteExpr(true);
+		    this.absParser.setTypeChecking(false);
 			parseProject();
 		}
 		
@@ -120,9 +119,6 @@ public class ProposalFactory{
 					}
 				});
 					// compile the current document with non-saved changes
-				        Main absParser = new Main();
-			                absParser.setAllowIncompleteExpr(true);
-			                absParser.setTypeChecking(false);
 					cu = absParser.parseUnit(file.getLocation().toFile(), null,
 							new StringReader(prepareDocContent()));
 					cu.setName(file.getLocation().toFile().getAbsolutePath());