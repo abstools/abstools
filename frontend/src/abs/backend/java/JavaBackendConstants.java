d4afc950d756568f78608163aa287554e53c3ba3
ywelsch <ywelsch@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2011-05-25 11:09:15 +0000

    removed unused packages in generated code
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@10673 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- frontend/src/abs/backend/java/JavaBackendConstants.java
+++ frontend/src/abs/backend/java/JavaBackendConstants.java
@@ -14,9 +14,8 @@ import abs.backend.java.lib.types.ABSType;
 public class JavaBackendConstants {
     public static String LIB_TYPES_PACKAGE = ABSType.class.getPackage().getName();
     public static String LIB_EXPR_PACKAGE = Let.class.getPackage().getName();
-    public static String LIB_RUNTIME_PACKAGE = Let.class.getPackage().getName();
     public static String LIB_IMPORT_STATEMENT = "import " + LIB_TYPES_PACKAGE + ".*; " + "import " + LIB_EXPR_PACKAGE
-            + ".*; " + "import " + LIB_RUNTIME_PACKAGE + ".*;";
+            + ".*;";
     public static String ABSRUNTIME = ABSRuntime.class.getName();
     public static String ANDGUARD = ABSAndGuard.class.getName();
     public static String CLAIMGUARD = ABSFutureGuard.class.getName();
