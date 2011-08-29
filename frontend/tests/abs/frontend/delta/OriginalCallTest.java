c3e6f9396295782032faffd301efa7289f8abe64
ramus <ramus@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2011-08-29 12:25:57 +0000

    fixing bug #270
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@11894 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- frontend/tests/abs/frontend/delta/OriginalCallTest.java
+++ frontend/tests/abs/frontend/delta/OriginalCallTest.java
@@ -20,26 +20,30 @@ public class OriginalCallTest extends DeltaFlattenerTest {
     public void originalCall() throws ASTNodeNotFoundException {
         Model model = assertParseOk(
                 "module M;"
-                + "class C { Unit m() {} }"
-                + "delta D { modifies class C { modifies Unit m() { original(); } } }"
+                + "interface I {}"
+                + "class C implements I { Unit m() {} }"
+                + "delta D { modifies class C implements I { modifies Unit m() { original(); } } }"
         );
         ClassDecl cls = (ClassDecl) findDecl(model, "M", "C");
         assertTrue(cls.getMethods().getNumChild() == 1);
         
         DeltaDecl delta = (DeltaDecl) findDecl(model, "M", "D");
-        assertTrue(delta.getClassOrIfaceModifiers().getNumChild() == 1);
+        assertTrue(delta.getNumClassOrIfaceModifier() == 1);
+        assertTrue(((ModifyClassModifier) delta.getClassOrIfaceModifier(0)).getNumModifier() == 1);
         
         model.resolveOriginalCalls(new ArrayList<DeltaDecl>(Arrays.asList(delta)));
-        assertTrue(delta.getClassOrIfaceModifiers().getNumChild() == 2);
+        assertTrue(delta.getNumClassOrIfaceModifier() == 2);
         
         model.applyDelta(delta);
         
-        // there should be two methods now: original and added-by-delta
+        // there should be two methods now: the original one and the one added by the delta
         assertTrue(cls.getMethods().getNumChild() == 2);
         assertTrue(cls.getMethod(0).getMethodSig().getName().equals("m"));
         assertTrue(cls.getMethod(1).getMethodSig().getName().startsWith("m_Orig"));
+        
+        // make sure the class still implements interface I
+        assertTrue(cls.getImplementedInterfaceUseList().getNumChild() == 1);
+        assertTrue(cls.getImplementedInterfaceUse(0).getName().equals("I"));
 
     }
-
-
 }
