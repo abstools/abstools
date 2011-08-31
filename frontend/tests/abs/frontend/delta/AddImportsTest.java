13c843582ab9d5bd9d1b04c2eb39d1eaf758651c
ramus <ramus@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2011-08-31 12:55:25 +0000

    more unit tests for the implicit addition of imports
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@11936 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- frontend/tests/abs/frontend/delta/AddImportsTest.java
+++ frontend/tests/abs/frontend/delta/AddImportsTest.java
@@ -4,14 +4,70 @@
  */
 package abs.frontend.delta;
 
+import java.util.Map;
+
 import org.junit.Test;
 import static org.junit.Assert.*;
 
-import abs.frontend.delta.exceptions.*;
 import abs.frontend.ast.*;
+import abs.frontend.typechecker.*;
+import abs.frontend.delta.exceptions.*;
 
 
 public class AddImportsTest extends DeltaFlattenerTest {
+    
+    @Test
+    public void addQualImport() throws ASTNodeNotFoundException {
+        Model model = assertParseOk(
+                "module M1; export *;"
+                + "class C {}"
+                
+                + "module M2; export *;"
+                + "interface I { Unit m(); }"
+                
+                + "module D;"
+                + "import * from M1;"
+                + "import * from M2;"
+                + "delta D { "
+                + "modifies class C implements M2.I { adds Unit m() {} } "
+                + "}"
+        );
+        ClassDecl cls = (ClassDecl) findDecl(model, "M1", "C");
+        DeltaDecl delta = (DeltaDecl) findDecl(model, "D", "D");
+        model.applyDelta(delta);
+
+        // the compiler needs to add an "import M2.I" to M1
+        ModuleDecl clsmodule = cls.getModule();
+        Map<KindedName, ResolvedName> clsVisibleSymbols = clsmodule.getVisibleNames();
+        KindedName symbol = new KindedName(KindedName.Kind.TYPE_DECL, "M2.I");
+        assertTrue(clsVisibleSymbols.containsKey(symbol));
+    }
+
+    @Test
+    public void addQualImport2() throws ASTNodeNotFoundException {
+        Model model = assertParseOk(
+                "module M; export *;"
+                + "interface I { Unit m(); }"
+                
+                + "module D;"
+                + "import * from M;"
+                + "delta D { "
+                + "adds class C implements M.I { Unit m() {} }"
+                + "}"
+        );
+        DeltaDecl delta = (DeltaDecl) findDecl(model, "D", "D");
+        model.applyDelta(delta);
+        
+        // the compiler doesn't need to add anything 
+        ClassDecl cls = (ClassDecl) findDecl(model, "D", "C");
+        ModuleDecl clsmodule = cls.getModule();
+        Map<KindedName, ResolvedName> clsVisibleSymbols = clsmodule.getVisibleNames();
+        KindedName symbol = new KindedName(KindedName.Kind.TYPE_DECL, "M.I");
+        assertTrue(clsVisibleSymbols.containsKey(symbol));
+        symbol = new KindedName(KindedName.Kind.TYPE_DECL, "I");
+        assertTrue(clsVisibleSymbols.containsKey(symbol));
+    }
+
     @Test
     public void addUnqualImport() throws ASTNodeNotFoundException {
         Model model = assertParseOk(
@@ -30,12 +86,12 @@ public class AddImportsTest extends DeltaFlattenerTest {
         );
         
         // the compiler needs to add an "import I from M2" to M1 
-
+        // TODO
+        
     }
-    
-    
+
     @Test
-    public void addQualImport() throws ASTNodeNotFoundException {
+    public void doNotAddImport() throws ASTNodeNotFoundException {
         Model model = assertParseOk(
                 "module M1; export *;"
                 + "class C {}"
@@ -43,36 +99,22 @@ public class AddImportsTest extends DeltaFlattenerTest {
                 + "module M2; export *;"
                 + "interface I { Unit m(); }"
                 
-                + "module MD;"
+                + "module D;"
                 + "import * from M1;"
-                + "import * from M2;"
                 + "delta D { "
                 + "modifies class C implements M2.I { adds Unit m() {} } "
                 + "}"
         );
         
-        // the compiler needs to add an "import M2.I" to M1 
+        ClassDecl cls = (ClassDecl) findDecl(model, "M1", "C");
+        DeltaDecl delta = (DeltaDecl) findDecl(model, "D", "D");
+        model.applyDelta(delta);
 
-    }
-
-    @Test
-    public void donotAddImport() throws ASTNodeNotFoundException {
-        Model model = assertParseOk(
-                "module M1; export *;"
-                + "class C {}"
-                
-                + "module M2; export *;"
-                + "interface I { Unit m(); }"
-                
-                + "module MD;"
-                + "import * from M1;"
-                + "delta D { "
-                + "modifies class C implements I { adds Unit m() {} } "
-                + "}"
-        );
-        
         // the compiler should not add an import, because the delta cannot see I!
-
+        ModuleDecl clsmodule = cls.getModule();
+        Map<KindedName, ResolvedName> clsVisibleSymbols = clsmodule.getVisibleNames();
+        KindedName symbol = new KindedName(KindedName.Kind.TYPE_DECL, "M2.I");
+        assertFalse(clsVisibleSymbols.containsKey(symbol));
     }
 
 }
