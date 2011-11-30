84c6a3bb35dfc452617a4d03aaaee445dc3a31e3
ramus <ramus@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2011-11-30 15:34:52 +0000

    a small example of using the FLI (FFI)
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@12967 97f52227-7fa9-4375-a72d-c1c733b1f48f

---
+++ frontend/tests/abssamples/fli/random-io/java/Random_fli.java
@@ -0,0 +1,17 @@
+package Env;
+
+import abs.backend.java.lib.types.*;
+import java.util.Random;
+
+public class Random_fli extends Random_c {
+    
+    public ABSInteger fli_random(ABSInteger max) {
+        Random rnd = new Random();
+        int n = rnd.nextInt(max.toInt());
+        return ABSInteger.fromInt(n);
+    }
+}
+
+/* useful:
+ * Tools/ABS/trunk/abs-foreign-interface/java/abs-java-util/src/main/java/abs/fli/java/PrimitiveUtil.java
+ */
