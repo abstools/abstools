7e8b49b40b2b2ab099bf32e893b52328fbcf7ccb
stolz <stolz@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2011-12-05 22:43:38 +0000

    - Whoops, don't forget to setup path to costabs_exe in #311 - Fix plugin data (missed after rename in r.12987)
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@13015 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- costabs-plugin/src/costabs/Activator.java
+++ costabs-plugin/src/costabs/Activator.java
@@ -1,8 +1,14 @@
 package costabs;
 
+import java.io.File;
+
+import org.eclipse.core.runtime.*;
 import org.eclipse.jface.resource.ImageDescriptor;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
+import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
+
+import costabs.console.CostabsShellCommand;
 
 /**
  * The activator class controls the plug-in life cycle
@@ -28,6 +34,12 @@ public class Activator extends AbstractUIPlugin {
 	public void start(BundleContext context) throws Exception {
 		super.start(context);
 		plugin = this;
+		Bundle petbundle = Platform.getBundle(PLUGIN_ID); 
+		File dir = FileLocator.getBundleFile(petbundle);
+		assert dir.isDirectory() : dir;
+		File exe = new File(dir,"costabs_exe");
+		assert exe.exists() : dir;
+		CostabsShellCommand.COSTABS_EXECUTABLE_PATH = exe.getAbsolutePath();
 	}
 
 	/*
@@ -57,5 +69,9 @@ public class Activator extends AbstractUIPlugin {
 	 */
 	public static ImageDescriptor getImageDescriptor(String path) {
 		return imageDescriptorFromPlugin(PLUGIN_ID, path);
+	}
+
+	public static void logException(Exception e) {
+		getDefault().getLog().log(new Status(IStatus.ERROR, PLUGIN_ID, e.getMessage(), e));
 	}
 }
