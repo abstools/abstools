43677f60795ef4cca4b31c33cad1f0ebb7dfc5bc
mzamalloa <mzamalloa@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2011-12-06 17:54:04 +0000

    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@13025 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- costabs-plugin/src/costabs/handlers/CostabsHandler.java
+++ costabs-plugin/src/costabs/handlers/CostabsHandler.java
@@ -97,7 +97,7 @@ public class CostabsHandler extends AbstractHandler {
 	}
 
 	private void callPrologBackend(String filename) throws Exception {
-    /*	int numArgs = 3;
+    	int numArgs = 3;
 		String[] args = new String[numArgs];
 		int i = 0;
 		args[i++] = "-d";
@@ -105,9 +105,9 @@ public class CostabsHandler extends AbstractHandler {
 		args[i++] = filename;
 
 		PrologBackend.runFromShell(args);
-		*/
-		Model model = CostabsLink.ABS_NATURE.getCompleteModel(); //getCurrentABSModel();
-		PrologBackend.runFromPlugin(model,"/tmp/costabs/absPL","abs.pl",CostabsLink.ENTRIES_NODES);
+		
+		//Model model = CostabsLink.ABS_NATURE.getCompleteModel(); //getCurrentABSModel();
+		//PrologBackend.runFromPlugin(model,"/tmp/costabs/absPL","abs.pl",CostabsLink.ENTRIES_NODES);
 	}
 	
 	/*
