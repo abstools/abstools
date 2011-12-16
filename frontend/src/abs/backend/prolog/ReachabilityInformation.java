02c4aee33c6da5c0e06b4bc6279159b5b98083ed
antonio <antonio@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2011-12-16 12:01:03 +0000

    Javadoc
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@13151 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- frontend/src/abs/backend/prolog/ReachabilityInformation.java
+++ frontend/src/abs/backend/prolog/ReachabilityInformation.java
@@ -22,16 +22,51 @@ import abs.frontend.ast.NewExp;
 import abs.frontend.ast.ParametricFunctionDecl;
 import abs.frontend.typechecker.InterfaceType;
 
+/**
+ * Auxiliary class for obtaining the reachable methods and functions of a given
+ * model from a given set of initial entries.
+ * It can record the processed elements and the reachable elements. It can
+ * also be queried about the reachability of an element.
+ * 
+ * @author aeflores
+ *
+ */
 public class ReachabilityInformation {
+    /**
+     * Records the method names that are reachable. The names can be:
+     * -Interface name+ method name
+     * -Class name+ method name
+     */
     private HashSet<String> reachableMethods;
+    /**
+     * Records the class or interface names that are reachable
+     */
     private HashSet<String> reachableInterfacesOrClasses;
+    /**
+     * Records the complete function nodes that are reachable and whether
+     * they have already been processed or not.
+     */
     private Hashtable<ASTNode<?>, Boolean> reachableFuncts;
+    /**
+     * Records the MainBlocks that are reachable. 
+     * (these will only be added by the constructor)
+     */
     private Hashtable<MainBlock, Boolean> reachableMainBlocks;
-
+    /**
+     * Records the method names that have been processed. The names can be:
+     * -Interface name+ method name
+     * -Class name+ method name
+     */
     private HashSet<String> processedMethods;
-
+    /**
+     * Flag that indicates if there has been any changes in the reachable sets.
+     */
     private boolean changed;
 
+    /**
+     * Initializes the databases with an initial set of elements.
+     * @param entries Initial set of reachable entries.
+     */
     public ReachabilityInformation(ArrayList<ASTNode<?>> entries) {
         // Sets creation
         reachableMethods = new HashSet<String>();
@@ -66,7 +101,9 @@ public class ReachabilityInformation {
 
         changed = true;
     }
-
+/*
+ * Methods that create standard name from the different combinations of elements
+ */
     private String getInterfaceId(InterfaceTypeUse inter) {
         return inter.getType().getQualifiedName();
     }
@@ -90,7 +127,11 @@ public class ReachabilityInformation {
     private String getMethodId(ClassDecl clazz, MethodSig method) {
         return getClassId(clazz) + method.getName();
     }
-
+/**
+ * finds the class where the given class belongs to
+ * @param node 
+ * @return the class that owns the parameter node or null if node does not belong to any class
+ */
     private ClassDecl ObtainOwnerClass(ASTNode<?> node) {
         ASTNode<?> ancestor;
         boolean foundClass = false;
@@ -106,7 +147,10 @@ public class ReachabilityInformation {
         }
         return (ClassDecl) ancestor;
     }
-
+/**
+ * returns true if there has been any change since the last time it was executed
+ * @return 
+ */
     public boolean changed() {
         if (changed) {
             changed = false;
@@ -114,24 +158,42 @@ public class ReachabilityInformation {
         } else
             return false;
     }
-
+/**
+ * checks if the parameter is reachable
+ * @param funct
+ * @return true if funct is reachable, false otherwise
+ */
     public boolean isReachable(FunctionDecl funct) {
         return reachableFuncts.containsKey(funct);
     }
-
+    /**
+     * checks if the parameter is reachable
+     * @param funct
+     * @return true if funct is reachable, false otherwise
+     */
     public boolean isReachable(ParametricFunctionDecl funct) {
         return reachableFuncts.containsKey(funct);
     }
-
+    /**
+     * checks if the parameter is reachable
+     * @param mBlock
+     * @return true if mBlock is reachable, false otherwise
+     */
     public boolean isReachable(MainBlock mBlock) {
         return reachableMainBlocks.containsKey(mBlock);
     }
-
+/**
+ * checks if the class is reachable. It looks at the class name
+ * and all the interface names that are directly or indirectly
+ * implemented by clazz
+ * @param clazz
+ * @return true if clazz is reachable, false otherwise
+ */
     public boolean isReachable(ClassDecl clazz) {
         boolean reachable = false;
         abs.frontend.ast.List<InterfaceTypeUse> interfaces = clazz.getImplementedInterfaceUseList();
         Iterator<InterfaceTypeUse> it = interfaces.iterator();
-
+        //checks if any interface implemented by clazz is reachable
         reachable = reachableInterfacesOrClasses.contains(getClassId(clazz));
         while (!reachable && it.hasNext()) {
             reachable = isReachable(it.next());
@@ -142,9 +204,10 @@ public class ReachabilityInformation {
 
     private boolean isReachable(InterfaceTypeUse inter) {
         boolean reachable = false;
+        //checks if inter is reachable
         reachable = reachableInterfacesOrClasses.contains(getInterfaceId(inter));
         if (!reachable) {
-
+            //checks if any interface implemented by inter is reachable
             abs.frontend.ast.List<InterfaceTypeUse> interfaces = ((InterfaceDecl) inter.getDecl())
                     .getExtendedInterfaceUseList();
             Iterator<InterfaceTypeUse> it = interfaces.iterator();
@@ -154,7 +217,13 @@ public class ReachabilityInformation {
         }
         return reachable;
     }
-
+    /**
+     * checks if the method is reachable. It looks at the class name
+     * and all the interface names that are directly or indirectly
+     * implemented by the method's class
+     * @param method
+     * @return true if method is reachable, false otherwise
+     */
     public boolean isReachable(MethodImpl method) {
         ClassDecl clazz = ObtainOwnerClass(method);
         boolean reachable = false;
@@ -162,7 +231,10 @@ public class ReachabilityInformation {
         if (clazz != null) {
             abs.frontend.ast.List<InterfaceTypeUse> interfaces = clazz.getImplementedInterfaceUseList();
             Iterator<InterfaceTypeUse> it = interfaces.iterator();
+            //checks if the method is reachable with its class name
             reachable = reachableMethods.contains(getMethodId(clazz, method.getMethodSig()));
+          //checks if the method is reachable with any interface name that is 
+          //implemented by its class
             while (!reachable && it.hasNext())
                 reachable = isReachable(it.next(), method.getMethodSig());
             return reachable;
@@ -172,8 +244,11 @@ public class ReachabilityInformation {
 
     private boolean isReachable(InterfaceTypeUse inter, MethodSig method) {
         boolean reachable = false;
+        //checks if the method is reachable with inter
         reachable = reachableMethods.contains(getMethodId(inter, method));
         if (!reachable) {
+            //checks if the method is reachable with any interface that is 
+            //implemented by inter
             abs.frontend.ast.List<InterfaceTypeUse> interfaces = ((InterfaceDecl) inter.getDecl())
                     .getExtendedInterfaceUseList();
             Iterator<InterfaceTypeUse> it = interfaces.iterator();
@@ -183,7 +258,12 @@ public class ReachabilityInformation {
         }
         return reachable;
     }
-
+    /**
+     * checks if the constructor is reachable, it is treated as a special
+     * method called init.
+     * @param block
+     * @return true if block is reachable, false otherwise
+     */
     public boolean isReachable(InitBlock block) {
         ClassDecl clazz = ObtainOwnerClass(block);
         if (clazz != null)
@@ -192,20 +272,36 @@ public class ReachabilityInformation {
             return false;
     }
 
-    // True is returned if it was not processed before
-    // that is, if the boolean value was false
+ 
+    /**
+     * Sets the parameter to processed
+     * @param funct
+     * @return true if the parameter had not been processed before.
+     */
     public boolean setProcessed(FunctionDecl funct) {
         return !reachableFuncts.put(funct, Boolean.TRUE);
     }
-
+    /**
+     * Sets the parameter to processed
+     * @param funct
+     * @return true if the parameter had not been processed before.
+     */
     public boolean setProcessed(ParametricFunctionDecl funct) {
         return !reachableFuncts.put(funct, Boolean.TRUE);
     }
-
+    /**
+     * Sets the parameter to processed
+     * @param mBlock
+     * @return true if the parameter had not been processed before.
+     */
     public boolean setProcessed(MainBlock mBlock) {
         return !reachableMainBlocks.put(mBlock, Boolean.TRUE);
     }
-
+    /**
+     * Sets the parameter to processed
+     * @param method
+     * @return true if the parameter had not been processed before.
+     */
     public boolean setProcessed(MethodImpl method) {
         ClassDecl clazz = ObtainOwnerClass(method);
         if (clazz != null)
@@ -213,7 +309,11 @@ public class ReachabilityInformation {
         else
             return false;
     }
-
+    /**
+     * Sets the parameter to processed
+     * @param block
+     * @return true if the parameter had not been processed before.
+     */
     public boolean setProcessed(InitBlock block) {
         ClassDecl clazz = ObtainOwnerClass(block);
         if (clazz != null)
@@ -221,7 +321,11 @@ public class ReachabilityInformation {
         else
             return false;
     }
-
+/**
+ *Sets the parameter to reachable and not processed if it was not reachable before
+ * @param funct
+ * @return true if the parameter was not reachable before
+ */
     public boolean addReachability(FunctionDecl funct) {
         if (!reachableFuncts.containsKey(funct)) {
             reachableFuncts.put(funct, Boolean.FALSE);
@@ -230,7 +334,11 @@ public class ReachabilityInformation {
         }
         return false;
     }
-
+    /**
+     *Sets the parameter to reachable and not processed if it was not reachable before
+     * @param funct
+     * @return true if the parameter was not reachable before
+     */
     public boolean addReachability(ParametricFunctionDecl funct) {
         if (!reachableFuncts.containsKey(funct)) {
             reachableFuncts.put(funct, Boolean.FALSE);
@@ -239,7 +347,11 @@ public class ReachabilityInformation {
         }
         return false;
     }
-
+    /**
+     *Sets the parameter to reachable and not processed if it was not reachable before
+     * @param inter
+     * @return true if the parameter was not reachable before
+     */
     public boolean addReachability(InterfaceType inter) {
         if (reachableInterfacesOrClasses.add(getInterfaceId(inter))) {
             changed = true;
@@ -247,7 +359,11 @@ public class ReachabilityInformation {
         }
         return false;
     }
-
+    /**
+     *Sets the parameter to reachable and not processed if it was not reachable before
+     * @param clazz
+     * @return true if the parameter was not reachable before
+     */
     public boolean addReachability(ClassDecl clazz) {
         if (reachableInterfacesOrClasses.add(getClassId(clazz))) {
             changed = true;
@@ -255,7 +371,12 @@ public class ReachabilityInformation {
         }
         return false;
     }
-
+    /**
+     *Sets the method to reachable using the interface inter.
+     * @param inter
+     * @param method
+     * @return true if the parameter was not reachable before
+     */
     public boolean addReachability(InterfaceType inter, MethodSig method) {
         if (reachableMethods.add(getMethodId(inter, method))) {
             changed = true;
@@ -263,7 +384,12 @@ public class ReachabilityInformation {
         }
         return false;
     }
-
+    /**
+     *Sets the method to reachable using the class clazz.
+     * @param clazz
+     * @param method
+     * @return true if the parameter was not reachable before
+     */
     public boolean addReachability(ClassDecl clazz, MethodSig method) {
         if (reachableMethods.add(getMethodId(clazz, method))) {
             changed = true;
@@ -272,7 +398,12 @@ public class ReachabilityInformation {
         return false;
 
     }
-
+    /**
+     *Sets the corresponding constructor to reachable.
+     * @param clazz
+     * @param newExp only uses to distinguish the method form others
+     * @return true if the parameter was not reachable before
+     */
     public boolean addReachability(ClassDecl clazz, NewExp newExp) {
         if (reachableMethods.add(getClassId(clazz) + "init")) {
             changed = true;
