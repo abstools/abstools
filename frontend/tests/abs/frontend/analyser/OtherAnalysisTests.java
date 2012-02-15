2c66a29f329a8516b58fc28cfedcb5a6e9563708
stolz <stolz@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2012-02-15 13:51:16 +0000

    There's something wrong with copying a model after type-checking. Document as test-case.
    Bonus points if you can figure out why the EXECUTION ORDER of the test cases MATTERS?!
    
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@13838 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- frontend/tests/abs/frontend/analyser/OtherAnalysisTests.java
+++ frontend/tests/abs/frontend/analyser/OtherAnalysisTests.java
@@ -4,12 +4,11 @@
  */
 package abs.frontend.analyser;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
 import abs.frontend.FrontendTest;
-import abs.frontend.ast.Exp;
 import abs.frontend.ast.Model;
 
 public class OtherAnalysisTests extends FrontendTest {
@@ -24,6 +23,32 @@ public class OtherAnalysisTests extends FrontendTest {
     public void finalTest() {
         assertParse("interface I { } { [Final] I i; i = null; }", Config.TYPE_CHECK, Config.WITH_STD_LIB, Config.EXPECT_TYPE_ERROR);
     }
-    
 
+    @Test
+    public void fullcopyTest() {
+        Model m = assertParseOk("module M; class C {}", Config.WITH_STD_LIB);
+        Model m2 = m.fullCopy();
+        assertFalse(m.hasErrors());
+        assertFalse(m2.hasErrors());
+    }
+
+    @Test
+    public void fullcopyTest1() {
+        Model m = assertParseOk("module M; class C {}", Config.WITH_STD_LIB);
+        Model m2 = m.fullCopy();
+        assertFalse(m.hasErrors());
+        assertFalse(m2.hasErrors());
+        assertTrue(m.typeCheck().isEmpty());
+        assertTrue(m2.typeCheck().isEmpty());
+    }
+
+    @Test
+    public void fullcopyTest2() {
+        Model m = assertParseOk("module M; class C {}", Config.WITH_STD_LIB);
+        assertFalse(m.hasErrors());
+        assertTrue(m.typeCheck().toString(),m.typeCheck().isEmpty());
+        Model m2 = m.fullCopy();
+        assertFalse(m2.hasErrors());
+        assertTrue(m2.typeCheck().toString(),m2.typeCheck().isEmpty());
+    }
 }
