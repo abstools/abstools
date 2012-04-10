b892d3b915115414f6cdb8b60fdf0f9b22c2f40a
woner <woner@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2012-04-10 14:02:02 +0000

    Update Java impl of ABS-FFI to be built using ecj and to not run javadoc.
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@14984 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- abs-foreign-interface/java/abs-java-util/src/main/java/abs/fli/java/io/System.java
+++ abs-foreign-interface/java/abs-java-util/src/main/java/abs/fli/java/io/System.java
@@ -1,5 +1,7 @@
 package abs.fli.java.io;
 
+import java.io.IOException;
+
 import FLI.SystemUtils.System_c;
 import abs.backend.java.lib.types.ABSString;
 import abs.backend.java.lib.types.ABSUnit;
@@ -14,6 +16,17 @@ public class System extends System_c {
     public ABSUnit fli_outPrintln(ABSString s) {
         java.lang.System.out.println(s.getString());
         return ABSUnit.UNIT;
+    }
+    
+    public ABSString fli_read() {
+        char c = 0;
+        try {
+            c = (char) java.lang.System.in.read();
+        } catch (IOException e) {
+            // print stack trace for now...
+            e.printStackTrace();
+        }
+        return ABSString.fromString(String.valueOf(c));
     }
 
 }
