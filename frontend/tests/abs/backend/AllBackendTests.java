4e1cf4b2ffaa41256f918c681072f790bf452b78
fkuehn <fkuehn@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2012-05-11 10:23:03 +0000

    Added the PrettyPrinter tests to the AllBackendTests
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@15313 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- frontend/tests/abs/backend/AllBackendTests.java
+++ frontend/tests/abs/backend/AllBackendTests.java
@@ -10,9 +10,10 @@ import org.junit.runners.Suite;
 import abs.backend.common.*;
 import abs.backend.java.*;
 import abs.backend.maude.MaudeTests;
+import abs.backend.prettyprint.PrettyPrinterTests;
 import abs.backend.tests.ASTBasedABSTestRunnerGeneratorTest;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ AllJavaTests.class, AllSemanticTests.class, MaudeTests.MaudeEqTests.class, MaudeTests.MaudeRlTests.class, ASTBasedABSTestRunnerGeneratorTest.class })
+@Suite.SuiteClasses({ AllJavaTests.class, AllSemanticTests.class, MaudeTests.MaudeEqTests.class, MaudeTests.MaudeRlTests.class, ASTBasedABSTestRunnerGeneratorTest.class, PrettyPrinterTests.class})
 public class AllBackendTests {
 }
