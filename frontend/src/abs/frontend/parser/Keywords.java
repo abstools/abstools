3fe895f1f64c38b58e36cf91980ad9253fcd7a18
stolz <stolz@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2012-07-20 09:24:51 +0000

    Add new keyword "uses" (#300).
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@16107 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- frontend/src/abs/frontend/parser/Keywords.java
+++ frontend/src/abs/frontend/parser/Keywords.java
@@ -4,23 +4,14 @@
  */
 package abs.frontend.parser;
 
-
-
 /**
-
  * This class is currently used by the Eclipse-Plugin as we have not found yet a
-
  * better way to get a list of all keywords. The list must be hold consistent
-
  * with the list defined in the ABS.flex file manually
-
  * 
-
  */
 
 public class Keywords {
-
-
 
     // This list has to be consistent with keywords in ABS.flex
 
@@ -37,6 +28,7 @@ public class Keywords {
         "def", 
         "implements", 
         "delta", 
+        "uses",
         "adds", 
         "modifies", 
         "removes", 
@@ -84,7 +76,6 @@ public class Keywords {
         "require" };
 
     
-
     public static String[] getKeywords() {
 
         return keywords;
