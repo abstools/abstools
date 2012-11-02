134364e11aa3f2a8921f3eb4fe50d40e870f24f8
stolz <stolz@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2012-11-02 08:46:08 +0000

    Refactor a few odds and ends around the type hierarchy.
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@17340 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- frontend/src/abs/frontend/analyser/TypeHierarchyHelper.java
+++ frontend/src/abs/frontend/analyser/TypeHierarchyHelper.java
@@ -5,6 +5,7 @@
 package abs.frontend.analyser;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
@@ -13,6 +14,7 @@ import java.util.Set;
 import abs.frontend.ast.ClassDecl;
 import abs.frontend.ast.CompilationUnit;
 import abs.frontend.ast.Decl;
+import abs.frontend.ast.HasTypeHierarchy;
 import abs.frontend.ast.InterfaceDecl;
 import abs.frontend.ast.InterfaceTypeUse;
 import abs.frontend.ast.Model;
@@ -25,16 +27,9 @@ public class TypeHierarchyHelper {
         Model model = interfaceDecl.getModel();
         // iterate over all the types
         // could be done more intelligent, when more performance is needed
-        for (CompilationUnit cu : model.getCompilationUnits()) {
-            for (ModuleDecl module : cu.getModuleDecls()) {
-                for (Decl decl : module.getDeclList()) {
-                    for (InterfaceDecl superType : getDirectSuperTypes(decl)) {
-                        if (superType == interfaceDecl) {
-                            result.add(decl);
-                        }
-                    }
-                }
-            }
+        for (Decl decl : model.getDecls()) {
+            if (decl.getDirectSuperTypes().contains(interfaceDecl))
+                result.add(decl);
         }
         return result;     
     }
@@ -44,48 +39,30 @@ public class TypeHierarchyHelper {
         Model model = interfaceDecl.getModel();
         // iterate over all the types
         // could be done more intelligent, when more performance is needed
-        for (CompilationUnit cu : model.getCompilationUnits()) {
-            for (ModuleDecl module : cu.getModuleDecls()) {
-                for (Decl decl : module.getDeclList()) {
-                    for (InterfaceDecl superType : getSuperTypes(decl)) {
-                        if (superType == interfaceDecl) {
-                            result.add(decl);
-                        }
-                    }
-                }
-            }
+        for (Decl decl : model.getDecls()) {
+            if (getSuperTypes(decl).contains(interfaceDecl))
+                result.add(decl);
         }
         return result;     
     }
 
-    public static List<InterfaceDecl> getDirectSuperTypes(Decl decl) {
-        if (decl instanceof InterfaceDecl) {
-            InterfaceDecl i = (InterfaceDecl) decl;
-            return getTypeDecls(i.getExtendedInterfaceUseList());
-        } else if (decl instanceof ClassDecl) {
-            ClassDecl c = (ClassDecl) decl;
-            return getTypeDecls(c.getImplementedInterfaceUseList());
-        } else {
-            return Collections.emptyList();
-        }
+    private static Set<InterfaceDecl> getSuperTypes(Decl decl) {
+        return addSuperTypes(new HashSet<InterfaceDecl>(), decl);
     }
 
-    public static Set<InterfaceDecl> getSuperTypes(Decl decl) {
-        Set<InterfaceDecl> superTypes = new HashSet<InterfaceDecl>();
-        addSuperTypes(superTypes, decl);
+    public static Set<InterfaceDecl> addSuperTypes(Set<InterfaceDecl> superTypes, Decl decl) {
+        if (decl instanceof HasTypeHierarchy) {
+            for (InterfaceDecl direct : decl.getDirectSuperTypes()) {
+                boolean isNew = superTypes.add(direct);
+                if (isNew) {
+                    addSuperTypes(superTypes, direct);
+                }
+            }
+        }
         return superTypes;
     }
 
-    private static void addSuperTypes(Set<InterfaceDecl> superTypes, Decl decl) {
-        for (InterfaceDecl direct : getDirectSuperTypes(decl)) {
-            boolean isNew = superTypes.add(direct);
-            if (isNew) {
-                addSuperTypes(superTypes, direct);
-            }
-        }
-    }
-
-    private static List<InterfaceDecl> getTypeDecls(Iterable<InterfaceTypeUse> extendedInterfaceUseList) {
+    public static List<InterfaceDecl> getTypeDecls(Iterable<InterfaceTypeUse> extendedInterfaceUseList) {
         List<InterfaceDecl> result = new ArrayList<InterfaceDecl>(); 
         for (InterfaceTypeUse extended : extendedInterfaceUseList) {
             Decl decl = extended.getDecl();
