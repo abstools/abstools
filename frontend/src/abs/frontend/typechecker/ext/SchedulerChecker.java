b7143c8c182926a69a0d816e333452c21108ee81
rschlatte <rschlatte@97f52227-7fa9-4375-a72d-c1c733b1f48f>
2012-11-29 17:10:49 +0000

    check scheduler annotation for `new' expressions
    git-svn-id: https://repos.hats-project.eu:444/svn/hats/Tools/ABS/trunk@17540 97f52227-7fa9-4375-a72d-c1c733b1f48f

--- frontend/src/abs/frontend/typechecker/ext/SchedulerChecker.java
+++ frontend/src/abs/frontend/typechecker/ext/SchedulerChecker.java
@@ -7,10 +7,16 @@ package abs.frontend.typechecker.ext;
 import abs.common.CompilerUtils;
 import abs.frontend.analyser.ErrorMessage;
 import abs.frontend.analyser.TypeError;
+import abs.frontend.ast.AssignStmt;
+import abs.frontend.ast.ASTNode;
 import abs.frontend.ast.ClassDecl;
-import abs.frontend.ast.Model;
-import abs.frontend.ast.PureExp;
+import abs.frontend.ast.Exp;
+import abs.frontend.ast.ExpressionStmt;
 import abs.frontend.ast.FnApp;
+import abs.frontend.ast.Model;
+import abs.frontend.ast.NewExp;
+import abs.frontend.ast.PureExp;
+import abs.frontend.ast.VarDeclStmt;
 import abs.frontend.typechecker.DataTypeType;
 import abs.frontend.typechecker.Type;
 
@@ -22,22 +28,53 @@ public class SchedulerChecker extends DefaultTypeSystemExtension {
         super(m);
     }
 
+    private void checkScheduleExp(PureExp sched, ASTNode loc) {
+        if (sched == null) return;
+        if (!(sched instanceof FnApp)) {
+            errors.add(new TypeError(loc, ErrorMessage.WRONG_SCHEDULER_TYPE,
+                                     sched.getType()));
+        } else {
+            FnApp s = (FnApp) sched;
+            Type t = s.getType();
+            if (!(t.isDataType() && ((DataTypeType)t).getSimpleName().equals("Process"))) {
+                errors.add(new TypeError(loc, ErrorMessage.WRONG_SCHEDULER_TYPE,
+                                         sched.getType()));
+            }
+            // TODO: type-check arguments of function call
+        }
+    }
+
     @Override
     public void checkClassDecl(ClassDecl decl) {
         PureExp sched = CompilerUtils.getAnnotationValue(decl.getAnnotations(), "Scheduler");
-        if (sched != null) {
-            if (!(sched instanceof FnApp)) {
-                errors.add(new TypeError(decl, ErrorMessage.WRONG_SCHEDULER_TYPE,
-                                         sched.getType()));
-            } else {
-                FnApp s = (FnApp) sched;
-                Type t = s.getType();
-                if (!(t.isDataType() && ((DataTypeType)t).getSimpleName().equals("Process"))) {
-                    errors.add(new TypeError(decl, ErrorMessage.WRONG_SCHEDULER_TYPE,
-                            sched.getType()));
-                }
-                // TODO: type-check arguments of function call
-            }
+        checkScheduleExp(sched, decl);
+    }
+
+    @Override
+    public void checkExpressionStmt(ExpressionStmt e) {
+        Exp exp = e.getExp();
+        if (exp instanceof NewExp) {
+            PureExp sched = CompilerUtils.getAnnotationValue(e.getAnnotations(), "Scheduler");
+            checkScheduleExp(sched, e);
+        }
+    }
+    
+    @Override
+    public void checkAssignStmt(AssignStmt s) {
+        Exp exp = s.getValue();
+        if (exp instanceof NewExp) {
+            PureExp sched = CompilerUtils.getAnnotationValue(s.getAnnotations(), "Scheduler");
+            checkScheduleExp(sched, s);
+        }
+    }
+
+    @Override
+    public void checkVarDeclStmt(VarDeclStmt v) {
+        if (!v.getVarDecl().hasInitExp()) return;
+        Exp exp = v.getVarDecl().getInitExp();
+        if (exp instanceof NewExp) {
+            PureExp sched = CompilerUtils.getAnnotationValue(v.getAnnotations(), "Scheduler");
+            checkScheduleExp(sched, v);
         }
     }
 
