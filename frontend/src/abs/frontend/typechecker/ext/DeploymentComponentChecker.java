587dabe7a1dee1557072347cc7a3855bebd9fd7f
Rudi Schlatte <rudi@constantly.at>
2013-11-28 11:31:35 +0100

    Forbid deployment components within cogs
    Since DCs model machine instances etc., it doesn't make sense for them
    to be part of a cog

--- frontend/src/abs/frontend/typechecker/ext/DeploymentComponentChecker.java
+++ frontend/src/abs/frontend/typechecker/ext/DeploymentComponentChecker.java
@@ -6,16 +6,28 @@ package abs.frontend.typechecker.ext;
 
 import abs.common.CompilerUtils;
 import abs.frontend.analyser.ErrorMessage;
+import abs.frontend.analyser.SemanticError;
 import abs.frontend.analyser.TypeError;
 import abs.frontend.ast.ASTNode;
 import abs.frontend.ast.AssignStmt;
 import abs.frontend.ast.ExpressionStmt;
 import abs.frontend.ast.Model;
+import abs.frontend.ast.NewExp;
 import abs.frontend.ast.PureExp;
 import abs.frontend.ast.VarDeclStmt;
+import abs.frontend.typechecker.Type;
 
+/**
+ * @author rudi
+ *
+ * Checks for type of DC annotation (must be DeploymentComponent) and for creation of deployment components within another cog (not possible).
+ */
 public class DeploymentComponentChecker extends DefaultTypeSystemExtension {
 
+    private static boolean isDeploymentComponent(Type t) {
+        return t.getQualifiedName().equals("ABS.DC.DeploymentComponent") || t.getQualifiedName().equals("ABS.DC.DC");
+    }
+    
     protected DeploymentComponentChecker(Model m) {
         super(m);
     }
@@ -37,8 +49,16 @@ public class DeploymentComponentChecker extends DefaultTypeSystemExtension {
 
     private void checkDCCorrect(ASTNode<?> n, PureExp dc) {
         if (dc == null) return;
-        if (!dc.getType().getQualifiedName().equals("ABS.DC.DeploymentComponent") && !dc.getType().getQualifiedName().equals("ABS.DC.DC")) {
+        if (!isDeploymentComponent(dc.getType())) {
             errors.add(new TypeError(n, ErrorMessage.WRONG_DEPLOYMENT_COMPONENT, dc.getType().getQualifiedName()));
         }
     }
+    
+    @Override
+    public void checkNewExp(NewExp e) {
+        if (isDeploymentComponent(e.getType()) && !e.hasCog()) {
+            errors.add(new SemanticError(e, ErrorMessage.DEPLOYMENT_COMPONENT_NOT_COG, "dummy string to keep constructor happy"));
+        }
+    }
+      
 }
