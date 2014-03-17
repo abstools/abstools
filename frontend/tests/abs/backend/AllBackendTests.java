3b065fbadd68bdda985fdec57f20039ed98da0fa
Volker Stolz <stolz@ifi.uio.no>
2014-03-17 11:42:36 +0100

    Haskell tests, again.

--- frontend/tests/abs/backend/AllBackendTests.java
+++ frontend/tests/abs/backend/AllBackendTests.java
@@ -9,21 +9,13 @@ import org.junit.runners.Suite;
 
 import abs.backend.abs2haskell.ABS2HaskellExamplesTest;
 import abs.backend.common.AllSemanticTests;
-import abs.backend.erlang.AllErlangTests;
-import abs.backend.java.AllJavaTests;
-import abs.backend.maude.AnnotationsTests;
-import abs.backend.maude.MaudeTests;
-import abs.backend.prettyprint.PrettyPrinterTests;
-import abs.backend.tests.ASTBasedABSTestRunnerGeneratorTest;
 import abs.backend.tests.OtherCodeGenTests;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    /* Do not merge
-    AllJavaTests.class, AllSemanticTests.class, MaudeTests.MaudeEqTests.class,
-        MaudeTests.MaudeRlTests.class, AnnotationsTests.class, ASTBasedABSTestRunnerGeneratorTest.class,
-        PrettyPrinterTests.class, OtherCodeGenTests.class, AllErlangTests.class
-     */
+    /* Do not merge */
+    AllSemanticTests.class,
+    OtherCodeGenTests.class,
     ABS2HaskellExamplesTest.class
         /* This line intentionally left blank */
         })
--- frontend/tests/abs/backend/tests/OtherCodeGenTests.java
+++ frontend/tests/abs/backend/tests/OtherCodeGenTests.java
@@ -4,14 +4,9 @@
  */
 package abs.backend.tests;
 
-import static org.junit.Assert.*;
-
-import java.io.*;
-
 import org.junit.*;
 
 import abs.backend.abs2haskell.ABS2HaskellDriver;
-import abs.backend.erlang.ErlApp;
 import abs.frontend.typesystem.CaseStudyTypeChecking;
 
 public class OtherCodeGenTests extends CaseStudyTypeChecking {
@@ -21,36 +16,6 @@ public class OtherCodeGenTests extends CaseStudyTypeChecking {
         super(input);
     }
 
-    @Test @Override
-    /* CoreABS */
-    public void test() throws Exception {
-        super.test();
-        File outFile = File.createTempFile("absjunit-core", null);
-        outFile.deleteOnExit();
-        m.generateCoreABS(new PrintStream(new BufferedOutputStream(new FileOutputStream(outFile))));
-    }
-
-    @Test
-    public void testProlog() throws Exception {
-        super.test();
-        File outFile = File.createTempFile("absjunit-pl", null);
-        outFile.deleteOnExit();
-        m.generateProlog(new PrintStream(new BufferedOutputStream(new FileOutputStream(outFile))), null);
-    }
-
-    @Test
-    public void testErlang() throws Exception {
-        super.test();
-        File tmpD = new File(System.getProperty("java.io.tmpdir"));
-        assertTrue(tmpD.exists() && tmpD.canWrite());
-        File tmp = new File(tmpD,"tmp_erl");
-        tmp.mkdir();
-        tmp.deleteOnExit();
-        ErlApp ea = new ErlApp(tmp);
-        m.generateErlangCode(ea);
-        ea.close();
-    }
-    
     @Test
     public void testABS2Haskell() throws Exception {
         super.test();
