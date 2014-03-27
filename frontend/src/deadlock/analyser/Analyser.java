f083287dbd7d071239d9946b52a02456ad3a8fc3
agarcia <abel.garcia2@unibo.it>
2014-03-27 01:40:26 +0100

    Fixing error with verbose output

--- frontend/src/deadlock/analyser/Analyser.java
+++ frontend/src/deadlock/analyser/Analyser.java
@@ -111,7 +111,7 @@ public class Analyser {
 
     /* 2. Analyze the contract */
     
-    out.println("\n\n\n\n\n\n\n\n\n\n\n");
+    if(verbose) out.println("\n\n\n\n\n\n\n\n\n\n\n");
     Map<String, Term> cct = new HashMap<String, Term>();
     
     for(String k : methodMap.keySet()){
@@ -122,14 +122,14 @@ public class Analyser {
     Term contract;
     for(String k : cct.keySet()){
         contract = cct.get(k);
-        out.println("    \"" + k + "\": " + ((contract != null) ? (contract.toString()) : ("null")));
+        if(verbose) out.println("    \"" + k + "\": " + ((contract != null) ? (contract.toString()) : ("null")));
     }
     
-    out.println("\n\n\n\n\n\n\n\n\n\n\n");
+    if(verbose) out.println("\n\n\n\n\n\n\n\n\n\n\n");
     DASolver solver = new DASolver(df, cct, nbIteration/*TODO, out*/);
 
     solver.computeSolution();
-    out.println(solver.toString());
+    if(verbose) out.println(solver.toString());
             
     out.println("### LOCK INFORMATION RESULTED BY THE ANALYSIS ###\n");
     out.println("Saturation:                   " + solver.isSatured());
