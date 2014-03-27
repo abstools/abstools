7f1c39af8590f42c72fe1322349e3e8ddece25bb
Volker Stolz <stolz@ifi.uio.no>
2014-03-27 10:32:04 +0100

    More rollback tests.

--- frontend/tests/abs/backend/common/RollbackTests.java
+++ frontend/tests/abs/backend/common/RollbackTests.java
@@ -4,6 +4,7 @@
  */
 package abs.backend.common;
 
+import org.junit.Assume;
 import org.junit.Test;
 
 import abs.backend.BackendTestDriver;
@@ -12,6 +13,7 @@ public class RollbackTests extends SemanticTests {
 
     public RollbackTests(BackendTestDriver d) {
         super(d);
+        Assume.assumeTrue(d.hasRollbacks());        
     }
 
     @Test
@@ -27,10 +29,27 @@ public class RollbackTests extends SemanticTests {
                 +      "class C implements I { Bool r = True; Unit test(Bool fail) { I o2 = new C(); r = False; if (fail) abort \"\"; else o2.test(True); } Bool getR() { return this.r;}}\n"
                 +       "{ I o = new C(); Fut<Unit> f = o!test(False); await f?; Bool testresult = o.getR(); }");
     }
+
     @Test
     public void rollbackOverCall2() {
         assertEvalTrue("interface I { Unit test(Bool fail); Bool getR(); }\n"
                 +      "class C implements I { Bool r = True; Unit test(Bool fail) { I o2 = new C(); r = True; if (fail) abort \"\"; else o2!test(True); } Bool getR() { return this.r;}}\n"
                 +       "{ I o = new C(); Fut<Unit> f = o!test(False); await f?; Bool testresult = o.getR(); }");
+    }
+    
+    @Test
+    public void rollbackOverCall3() {
+        // Aborting while awaiting shouldn't change a thing.
+        assertEvalTrue("interface I { Unit test(Bool fail); Bool getR(); }\n"
+                +      "class C implements I { Bool r = True; Unit test(Bool fail) { I o2 = new C(); r = True; if (fail) abort \"\"; else await o2!test(True); } Bool getR() { return this.r;}}\n"
+                +       "{ I o = new C(); Fut<Unit> f = o!test(False); await f?; Bool testresult = o.getR(); }");
+    }
+
+    @Test
+    public void rollbackAwait() {
+        // Maude doesn't generate runtime errors for that (yet)
+        assertEvalTrue("interface I { Unit test(); Bool getR(); }\n"
+                +      "class C implements I { Bool r = True; Unit test() { r = False; await r && 1/0 > 0; } Bool getR() { return this.r;}}\n"
+                +       "{ I o = new C(); Fut<Unit> f = o!test(); await f?; Bool testresult = o.getR(); }");
     }
 }
