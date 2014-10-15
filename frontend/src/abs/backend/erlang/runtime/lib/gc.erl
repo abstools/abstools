5dcf8c13e4a77559e199acf9f294a1442e9349cd
Sigmund Hansen <sigmunha@ifi.uio.no>
2014-10-15 14:23:40 +0200

    Adds stopping the world to garbage collector.
    Removes timeout to start a garbage collection cycle, instead starting
    based on new objects being added.

--- frontend/src/abs/backend/erlang/runtime/lib/gc.erl
+++ frontend/src/abs/backend/erlang/runtime/lib/gc.erl
@@ -6,7 +6,7 @@
 
 -include_lib("log.hrl").
 -include_lib("abs_types.hrl").
--export([start/0, init/0, extract_references/1]).
+-export([start/0, init/0, extract_references/1, stop_world/1, resume_world/1, get_references/1]).
 
 -export([behaviour_info/1]).
 
@@ -21,29 +21,23 @@ init() ->
 
 loop(Cogs, Objects) ->
     ?DEBUG({{cogs, Cogs}, {objects, Objects}}),
-    receive
-        #cog{ref=Ref} ->
-            loop(gb_sets:add_element({cog, Ref}, Cogs), Objects);
-        #object{ref=Ref} ->
-            loop(Cogs, gb_sets:add_element({object, Ref}, Objects));
-        Ref when is_pid(Ref) ->
-            loop(Cogs, gb_sets:add_element({future, Ref}, Objects));
-        X ->
-            ?DEBUG({unknown_message, X}),
-            loop(Cogs, Objects)
-%    after 5 ->
-%            Sauce = gb_sets:fold(fun (X, Acc) ->
-%            ?DEBUG({sauce, Sauce}),
-%            Black = mark(Cogs, lists:partition(fun ({Obj, Stack}) ->
-%                                                       case Stack of
-%                                                           [] -> false;
-%                                                           _ -> true
-%                                                       end end,
-%                                               rpc:pmap({gc,get_references}, [], gb_sets:to_list(Objects)))),
-%            Black = gb_sets:to_list(Objects),
-%            ?DEBUG({black_set, Black}),
-%            loop(Cogs, Objects)
-    end.
+    {NewCogs, NewObjects} =
+        receive
+            #cog{ref=Ref} ->
+                {gb_sets:add_element({cog, Ref}, Cogs), Objects};
+            #object{ref=Ref} ->
+                {Cogs, gb_sets:add_element({object, Ref}, Objects)};
+            Ref when is_pid(Ref) ->
+                {Cogs, gb_sets:add_element({future, Ref}, Objects)};
+            X ->
+                ?DEBUG({unknown_message, X}),
+                {Cogs, Objects}
+        end,
+    CogList = gb_sets:to_list(NewCogs),
+    StopList = rpc:pmap({gc, stop_world}, [], CogList),
+    Gray = lists:foldl(fun ordsets:union/2, [], rpc:pmap({gc, get_references}, [], CogList)),
+    rpc:pmap({gc, resume_world}, [], CogList),
+    loop(NewCogs, NewObjects).
 
 mark(Cogs, {Gray, White}) ->
     ?DEBUG({collect, {gray, Gray}, {white, White}}),
@@ -51,16 +45,27 @@ mark(Cogs, {Gray, White}) ->
 
 mark(Cogs, Black, [], White) ->
     lists:reverse(Black);
-mark(Cogs, Black, [{Module,Ref}|Gray], White) ->
-    NewGrays = lists:filter(fun (O) -> not ordsets:is_element(O, Black) end, Module:get_references(Ref)),
-    mark(Cogs, [Ref|Black], ordsets:union(NewGrays, Gray), White).
+mark(Cogs, Black, [Object|Gray], White) ->
+    NewGrays = lists:filter(fun (O) -> not ordsets:is_element(O, Black) end, get_references(Object)),
+    mark(Cogs, [Object|Black], ordsets:union(NewGrays, Gray), White).
+
+stop_world({cog, Ref}) ->
+    cog:stop_world(Ref).
+
+resume_world({cog, Ref}) ->
+    cog:resume_world(Ref).
+
+get_references({Module, Ref}) ->
+    Module:get_references(Ref).
 
 extract_references(DataStructure) ->
-    lists:filter(fun ({Module, Pid}) -> true; (_) -> false end,
-                 lists:flatten([to_deep_list(DataStructure)])).
+    ordsets:from_list(lists:filter(fun ({Module, Pid}) -> true; (_) -> false end,
+                                   lists:flatten([to_deep_list(DataStructure)]))).
 
 to_deep_list(#object{ref=Ref}) ->
     {object, Ref};
+to_deep_list(#cog{}) ->
+    [];
 to_deep_list(Ref) when is_pid(Ref) ->
     {future, Ref};
 to_deep_list(DataStructure) when is_tuple(DataStructure) ->
