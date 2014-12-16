8b5b698ca86c181d9f3e87833abf29b01fe5a6d2
Rudi Schlatte <rudi@ifi.uio.no>
2014-12-16 17:17:11 +0100

    Document how to X within vagrant.
    Also install Emacs to have something to X.

--- vagrant-bootstrap.sh
+++ vagrant-bootstrap.sh
@@ -1,3 +1,8 @@
 # Support file for Vagrant.  Add any software you need installed here.
 apt-get update
-apt-get install -y default-jdk ant git erlang
+apt-get install -y default-jdk ant git erlang emacs
+
+# This downloads a complete eclipse + saco installation.  TBD: install
+# it, adjust paths, etc.  Also, add abs-mode to local .emacs
+
+# wget http://costa.ls.fi.upm.es/saco/sw/Eclipse-SACO.tgz
