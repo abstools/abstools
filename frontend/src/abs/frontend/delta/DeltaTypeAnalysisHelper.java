ad1056a177026b63cea1755700d027a329ca257a
Radu Muschevici <radu.muschevici@cs.kuleuven.be>
2014-12-27 23:19:16 +0100

    Extend CSP solver to obtain all valid products

--- frontend/src/abs/frontend/delta/DeltaTypeAnalysisHelper.java
+++ frontend/src/abs/frontend/delta/DeltaTypeAnalysisHelper.java
@@ -25,7 +25,7 @@ public class DeltaTypeAnalysisHelper {
     public static List<Set<String>> getDeltaPartition(ProductLine pl, SemanticErrorList l) {
 
         TopologicalSorting<String> sorter = new TopologicalSorting<String>(getAllDeltas(pl));
-        
+
         for (DeltaClause clause : pl.getDeltaClauses()) {
             String deltaid = clause.getDeltaspec().getName();
             for (DeltaID did : clause.getAfterDeltaIDs()) {
