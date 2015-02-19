dbd69035b7e437a388208ed26ade3cb67802e588
lienhardt <michael.lienhardt@inria.fr>
2015-02-19 14:57:37 +0100

    MAIN GENERATION: yet another new version of the annotations and generation

--- frontend/src/autodeploy/DeployInformationClass.java
+++ frontend/src/autodeploy/DeployInformationClass.java
@@ -12,100 +12,111 @@ public class DeployInformationClass {
   private Map<String, Integer> _cost;
   private List<DeployInformationClassSpecification> _spec;
 
-  private abs.frontend.ast.List<ParamDecl> _params;
+  private List<String> _paramList;        // parameter list, in order
+  private Map<String, String> _paramType; // type of every parameter
 
   public DeployInformationClass(abs.frontend.ast.List<ParamDecl> l) {
     _cost = new HashMap<String, Integer>();
     _spec = new LinkedList<DeployInformationClassSpecification>();
-    _params = l;
+
+    _paramList = new LinkedList<String>();
+    _paramType = new HashMap<String, String>();
+
+
+    for(ParamDecl decl: l) { // init the list of parameters with their respective types
+      String param = decl.getName();
+      _paramList.add(param);
+      String port = decl.getType().getQualifiedName();
+      if(port == null) { System.out.println("Error: a port is null"); }
+      if(port.equals("ABS.StdLib.List"))
+        port = ((DataTypeType) (decl.getType())).getTypeArgs().get(0).getQualifiedName();
+      _paramType.put(param, port);
+    }
+
+    System.out.println("Scenario Specification found");
   }
-
-
-  private void addCost(String name, int cost) { _cost.put(name, new Integer(cost)); }
-  private void addRequirement(DeployInformationClassSpecification l) { _spec.add(l); }
-  //private void setProvide(int p) { _provide = p; }
 
 
   public void addAnn(PureExp exp) {
 
-    if(((DataConstructorExp)exp).getDataConstructor().getName().equals("Cost")) {
-      String name = ((StringLiteral) ((DataConstructorExp) exp).getParam(0)).getContent();
-      int cost = Integer.parseInt(((IntLiteral) ((DataConstructorExp) exp).getParam(1)).getContent());
-      System.out.println("Cost = (\"" + name + "\", " + cost + ")");
-      addCost(name, cost);
-    } else if(((DataConstructorExp)exp).getDataConstructor().getName().equals("Spec")) {
-      System.out.print("Spec = (");
-      DeployInformationClassSpecification spec = new DeployInformationClassSpecification();
-      int provide = Integer.parseInt(((IntLiteral) ((DataConstructorExp) exp).getParam(0)).getContent());
-      System.out.print(provide + ", [");
-      spec.setProvide(provide);
-      PureExp l = ((FnApp)(((DataConstructorExp) exp).getParam(1))).getParam(0); // list of declaration
-      Iterator<ParamDecl> iParam = _params.iterator();
-      while(((DataConstructorExp)l).hasParam() && (((DataConstructorExp)l).getParam(0) != null)) { // means we have a cons
-        PureExp el = ((DataConstructorExp)l).getParam(0);
-        l = ((DataConstructorExp)l).getParam(1);
-        if(((DataConstructorExp)el).getDataConstructor().getName().equals("Req")) {
+    // A specification is a list (hence a FnApp with first argument being a list)
+    DeployInformationClassSpecification info = new DeployInformationClassSpecification(_paramList, _paramType);
+    PureExp list = ((FnApp)exp).getParam(0);
+
+    while(((DataConstructorExp)list).hasParam() && (((DataConstructorExp)list).getParam(0) != null)) { // means we have a cons
+      PureExp el = ((DataConstructorExp)list).getParam(0);
+      list = ((DataConstructorExp)list).getParam(1);
+
+      if(((DataConstructorExp)el).getDataConstructor().getName().equals("Cost")) {
+        String name = ((StringLiteral) ((DataConstructorExp) el).getParam(0)).getContent();
+        int cost = Integer.parseInt(((IntLiteral) ((DataConstructorExp) el).getParam(1)).getContent());
+        info.addCost(name, cost);
+
+        System.out.println("  Annotation is Cost(\"" + name + "\", " + cost + ")");
+
+      } else if(((DataConstructorExp)el).getDataConstructor().getName().equals("MaxUse")) {
+        int arity = Integer.parseInt(((IntLiteral) ((DataConstructorExp) el).getParam(0)).getContent());
+        info.setProvide(arity);
+
+        System.out.println("  Annotation is MaxUse("  + arity + ")");
+
+      } else if(((DataConstructorExp)el).getDataConstructor().getName().equals("Name")) {
+        String name = ((StringLiteral) ((DataConstructorExp) el).getParam(0)).getContent();
+        info.addScenarioName(name);
+
+        System.out.println("  Annotation is Name("  + name + ")");
+
+      } else if(((DataConstructorExp)el).getDataConstructor().getName().equals("Param")) {
+        String param = ((StringLiteral) ((DataConstructorExp) el).getParam(0)).getContent();
+        String port = _paramType.get(param);
+        PureExp spec = ((DataConstructorExp)el).getParam(1);
+        if(((DataConstructorExp)spec).getDataConstructor().getName().equals("Req")) {
           System.out.print("Req");
-            String port = iParam.next().getType().getQualifiedName();
-            System.out.print("(\"" + port + "\")");
-            spec.addRequirement(port);
+          info.addRequirement(param);
 
-        } else if(((DataConstructorExp)el).getDataConstructor().getName().equals("List")) {
-          System.out.print("List");
-          int arity = Integer.parseInt(((IntLiteral) ((DataConstructorExp) el).getParam(0)).getContent());
-          String port = ((DataTypeType) (iParam.next().getType())).getTypeArgs().get(0).getQualifiedName();
-          if(port != null) System.out.print("(\"" + port + "\", " + arity + ")");
-          spec.addList(port, arity);
+          System.out.println("  Annotation is Req(\"" + param + "\")");
 
-        } else if(((DataConstructorExp)el).getDataConstructor().getName().equals("Default")) {
+        } else if(((DataConstructorExp)spec).getDataConstructor().getName().equals("List")) {
+          int arity = Integer.parseInt(((IntLiteral) ((DataConstructorExp) spec).getParam(0)).getContent());
+          info.addList(param, arity);
+
+          System.out.println("  Annotation is List(\"" + port + "\", " + arity + ")");
+
+        } else if(((DataConstructorExp)spec).getDataConstructor().getName().equals("Default")) {
           System.out.print("Default");
-          String value = ((StringLiteral) ((DataConstructorExp) el).getParam(0)).getContent();
-          String port = iParam.next().getType().getQualifiedName();
+          String value = ((StringLiteral) ((DataConstructorExp) spec).getParam(0)).getContent();
           if(port != null) System.out.print("(\"" + port + "\", " + value + ")");
-          spec.addDefault(port, value);
+          info.addDefault(param, value);
 
-        } else if(((DataConstructorExp)el).getDataConstructor().getName().equals("Fill")){
-          System.out.print("Fill");
-          ParamDecl p = iParam.next();
-          String port = p.getType().getQualifiedName();
-          String field = p.getName();
+          System.out.println("  Annotation is Default(\"" + port + "\", " + value + ")");
+
+        } else if(((DataConstructorExp)spec).getDataConstructor().getName().equals("User")){
+          System.out.print("User");
           if(port != null) System.out.print("(\"" + port + "\")");
-          spec.addFill(port, field);
+          info.addUser(param, port);
+
+          System.out.println("  Annotation is User(\"" + port + "\", " + param + ")");
 
         }
       }
-      System.out.println("])");
-      addRequirement(spec);
     }
+    _spec.add(info);
   }
 
   public void generateJSON(String className, PrintWriter f) {
     // 1: the cost that is shared between all the instances of that class
     f.write("    {\n");
-    f.write("      \"name\": \"" + className + "\",\n");
-    f.write("      \"cost\": {\n");
-    Iterator<Map.Entry<String, Integer>> iCost = _cost.entrySet().iterator();
-    while(iCost.hasNext()) {
-      Map.Entry<String, Integer> entry = iCost.next();
-      f.write("        \"" + entry.getKey() + "\": " + entry.getValue());
-      if(iCost.hasNext()) f.write(",\n");
-    }
-    f.write("\n      },\n");
-    f.write("      \"activates\": [\n");
+    f.write("      \"name\": \"" + className + "\",\n");f.write("      \"activates\": [\n");
 
     Iterator<DeployInformationClassSpecification> iSpec = _spec.iterator();
     while(iSpec.hasNext()) {
       DeployInformationClassSpecification spec = iSpec.next();
-      f.write("        {\n");
-      f.write("          \"provide\": " + spec.getProvide() + ",\n");
-      f.write("          \"sig\": [\n");
       spec.generateJSON(f);
-      f.write("          ]\n"); // end of the require
-      f.write("        }");
       if(iSpec.hasNext()) f.write(",\n");
     }
     f.write("\n      ]\n");
     f.write("    }");
   }
 
+  public boolean isEmpty() { return _spec.isEmpty(); }
 }
--- frontend/src/autodeploy/DeployInformationClassSpecification.java
+++ frontend/src/autodeploy/DeployInformationClassSpecification.java
@@ -6,61 +6,109 @@ import java.util.*;
 public class DeployInformationClassSpecification {
 
 
-
-  private interface IDeployInformationClassSpecificationRequirement { String toString(); }
+  private interface IDeployInformationClassSpecificationRequirement { String toString(String _port); }
 
   private class DeployInformationClassSpecificationRequirementReq implements  IDeployInformationClassSpecificationRequirement {
-    private String _port;
-    public DeployInformationClassSpecificationRequirementReq(String port) { _port = port; }
-    public String toString() { return "{ \"type\": \"require\", \"arity\": 1, \"value\": \"" + _port + "\" }"; }
+    public DeployInformationClassSpecificationRequirementReq() {  }
+    public String toString(String _port) { return "{ \"type\": \"require\", \"arity\": 1, \"value\": \"" + _port + "\" }"; }
   }
 
   private class DeployInformationClassSpecificationRequirementList implements  IDeployInformationClassSpecificationRequirement {
-    private String _port;
     private int _arity;
-    public DeployInformationClassSpecificationRequirementList(String port, int arity) { _port = port; _arity = arity; }
-    public String toString() { return "{ \"type\": \"list\", \"arity\": " + _arity + ", \"value\": \"" + _port + "\" }"; }
+    public DeployInformationClassSpecificationRequirementList(int arity) { _arity = arity; }
+    public String toString(String _port) { return "{ \"type\": \"list\", \"arity\": " + _arity + ", \"value\": \"" + _port + "\" }"; }
   }
 
   private class DeployInformationClassSpecificationRequirementDefault implements  IDeployInformationClassSpecificationRequirement {
-    private String _port;
     private String _value;
-    public DeployInformationClassSpecificationRequirementDefault(String port, String value) { _port = port; _value = value; }
-    public String toString() { return "{ \"type\": \"default\", \"arity\": 1, \"value\": \"" + _value + "\" }"; }
+    public DeployInformationClassSpecificationRequirementDefault(String value) { _value = value; }
+    public String toString(String _port) { return "{ \"type\": \"default\", \"arity\": 1, \"value\": \"" + _value + "\" }"; }
   }
 
-  private class DeployInformationClassSpecificationRequirementFill implements  IDeployInformationClassSpecificationRequirement {
-    private String _port;
-    private String _field;
-    public DeployInformationClassSpecificationRequirementFill(String port, String field) { _port = port; _field = field; }
-    public String toString() { return "{ \"type\": \"user\", \"arity\": 1, \"value\": \"" + _field + "\" }"; }
+  private class DeployInformationClassSpecificationRequirementUser implements  IDeployInformationClassSpecificationRequirement {
+    String _field;
+    public DeployInformationClassSpecificationRequirementUser(String field) { _field = field; }
+    public String toString(String _port) { return "{ \"type\": \"user\", \"arity\": 1, \"value\": \"" + _field + "\" }"; }
   }
 
 
-  private List<IDeployInformationClassSpecificationRequirement> _req;
+  private List<String> _paramList;
+  private Map<String, String> _paramType;
+
   private int _provide;
+  private Map<String, Integer> _cost;
+  private Map<String, IDeployInformationClassSpecificationRequirement> _params;
+  private Set<String> _names;
 
-  public DeployInformationClassSpecification() {_req = new LinkedList<IDeployInformationClassSpecificationRequirement>(); }
+  public DeployInformationClassSpecification(List<String> paramList, Map<String, String> paramType) {
+    _paramList = paramList;
+    _paramType = paramType;
+    _provide = -1;                   // per default, the provide is infinite
+    _cost = new HashMap<String, Integer>();
+    _params = new HashMap<String, IDeployInformationClassSpecificationRequirement>();
+    for(String param: _paramList) {  // per default, all fields must be filled by the user.
+      _params.put(param, new DeployInformationClassSpecificationRequirementUser(param));
+    }
+    _names = new HashSet<String>();
+  }
 
   public void setProvide(int p) { _provide = p; }
   public int getProvide() { return _provide; }
 
+  public void addCost(String resource, int cost) { _cost.put(resource, new Integer(cost)); }
+  public int getCost(String resource) {
+    Integer cost = _cost.get(resource);
+    if(cost == null) { return 0; }
+    else { return cost.intValue(); }
+  }
 
-  public void addRequirement(String port) { _req.add(new DeployInformationClassSpecificationRequirementReq(port)); }
-  public void addList(String port, int arity) { _req.add(new DeployInformationClassSpecificationRequirementList(port, arity)); }
-  public void addDefault(String port, String value) { _req.add(new DeployInformationClassSpecificationRequirementDefault(port, value)); }
-  public void addFill(String port, String field) { _req.add(new DeployInformationClassSpecificationRequirementFill(port, field)); }
+
+  public void addRequirement(String param) { _params.put(param, new DeployInformationClassSpecificationRequirementReq()); }
+  public void addList(String param, int arity) { _params.put(param, new DeployInformationClassSpecificationRequirementList(arity)); }
+  public void addDefault(String param, String value) { _params.put(param, new DeployInformationClassSpecificationRequirementDefault(value)); }
+  public void addUser(String param, String field) { _params.put(param, new DeployInformationClassSpecificationRequirementUser(field)); }
+  public void addScenarioName(String name) { _names.add(name); }
 
 
 
   public void generateJSON(PrintWriter f) {
-    Iterator<IDeployInformationClassSpecificationRequirement> iReq = _req.iterator();
+
+    f.write("        {\n");
+    // 1. Provide
+    f.write("          \"provide\": " + this.getProvide() + ",\n");
+    // 2. Cost
+    f.write("          \"cost\": {\n");
+    Iterator<Map.Entry<String, Integer>> iCost = _cost.entrySet().iterator();
+    while(iCost.hasNext()) {
+      Map.Entry<String, Integer> entry = iCost.next();
+      f.write("            \"" + entry.getKey() + "\": " + entry.getValue());
+      if(iCost.hasNext()) f.write(",\n");
+    }
+    f.write("\n         },\n");
+    // 3. Scenario names
+    f.write("          \"scenarios\": [ ");
+    Iterator<String> iName = _names.iterator();
+    while(iName.hasNext()) {
+      String name = iName.next();
+      f.write("\"" + name + "\"");
+      if(iName.hasNext()) f.write(", ");
+      else f.write(" ");
+    }
+    f.write("],\n");
+    // 4, requirements
+    f.write("          \"sig\": [\n");
+    Iterator<String> iReq = _paramList.iterator();
     while(iReq.hasNext()) {
+      String param = iReq.next();
       f.write("            ");
-      f.write(iReq.next().toString());
-      if(iReq.hasNext()) f.write(",");
+      f.write(_params.get(param).toString(_paramType.get(param)));
+      if(iReq.hasNext()) f.write(", ");
       f.write("\n");
     }
+    f.write("          ]\n"); // end of the require
+    f.write("        }");
+
+
   }
 
 
