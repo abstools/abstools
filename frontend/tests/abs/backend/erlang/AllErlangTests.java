8b583b1760727827aba7eaa7d025bb0f00d0ddb1
Rudi Schlatte <rudi@ifi.uio.no>
2015-04-29 17:31:26 +0200

    remove some outdated tests
    - `safeget`, `abort`, cog-level rollback are not in the main language.
      All exception-based tests should be put in
      `tests/abs/backend/common/ExceptionTests.java` (which currently fails;
      investigation ongoing)

--- frontend/tests/abs/backend/erlang/AllErlangTests.java
+++ frontend/tests/abs/backend/erlang/AllErlangTests.java
@@ -7,7 +7,7 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ ErlangErrorHandlingTests.class, ErlangExamplesTests.class })
+@Suite.SuiteClasses({ ErlangExamplesTests.class })
 public class AllErlangTests {
 
 }
