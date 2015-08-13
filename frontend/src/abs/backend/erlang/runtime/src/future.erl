43652c2cca654f7486e6e0455b8d2b9f73403fbe
Rudi Schlatte <rudi@ifi.uio.no>
2015-08-13 11:21:42 +0200

    Eliminate spurious clock advances
    - do not allow a process to terminate before processes waiting for the
      result future have woken up
    
    - notify processes awaiting on future to avoid spurious "everyone is
      idle" moments.  The callee's cog would announce it was idle before the
      cog(s) waiting on its future announced that they were now running
      again; this would lead to spurious clock advances.
    
    - also remove various gc deadlocks: when the process suspended itself
      at the same time the cog tried to stop it for gc, the stop_world
      signal would linger until the next safepoint, stopping the process at
      unexpected times.

--- frontend/src/abs/backend/erlang/runtime/src/future.erl
+++ frontend/src/abs/backend/erlang/runtime/src/future.erl
@@ -1,7 +1,7 @@
 %%This file is licensed under the terms of the Modified BSD License.
 -module(future).
 -export([init/4,start/5]).
--export([get_after_await/1,get_blocking/3,await/3,poll/1,die/2]).
+-export([get_after_await/1,get_blocking/3,await/3,poll/1,die/2,complete/4]).
 -include_lib("abs_types.hrl").
 -include_lib("log.hrl").
 %%Future starts AsyncCallTask
@@ -29,6 +29,24 @@ start(Callee,Method,Params,CurrentCog,Stack) ->
     end)(),
     Ref.
 
+complete(Ref, Value, Sender, Cog) ->
+    Ref!{completed, Value, Sender, Cog},
+    (fun Loop() ->
+             %% Wait for message to be received, but handle GC request in the
+             %% meantime.
+             receive
+                 {stop_world, _Sender} ->
+                     task:block_for_gc(Cog),
+                     %% this runs in the context of the just-completed task,
+                     %% so we only need to hold on to the return value.
+                     task:acquire_token(Cog, [Value]),
+                     Loop();
+                {get_references, Sender} ->
+                    Sender ! {gc:extract_references([Value]), self()},
+                    Loop();
+                {ok, Ref} -> ok
+            end
+    end)().
 
 get_after_await(Ref)->
     Ref!{get,self()},
@@ -44,6 +62,10 @@ get_blocking(Ref, Cog, Stack) ->
     Ref ! {get, self()},
     Result = (fun Loop() ->
                       receive
+                         {stop_world, _Sender} ->
+                             %% we already blocked above.  Eat the message or
+                             %% we'll block at inopportune moments later.
+                             Loop();
                           {get_references, Sender} ->
                               Sender ! {gc:extract_references(Stack), self()},
                               Loop();
@@ -67,13 +89,32 @@ await(Ref, Cog, Stack) ->
             task:release_token(Cog, waiting),
             (fun Loop() ->
                      receive
-                         {ok} -> ok;
+                         {ok, Ref, Cog} ->
+                             %% It's an async self-call; unblock the callee
+                             %% before we try to acquire the token ourselves.
+                             Ref ! {okthx, self()},
+                             task:acquire_token(Cog, Stack);
+                         {ok, Ref, _CalleeCog} ->
+                             %% It's a call to another cog: get our cog to
+                             %% running status before allowing the other cog
+                             %% to idle
+
+                             %% TODO: this blocks the other cog until we are
+                             %% scheduled.  Investigate whether something like
+                             %% "cog:new_state(Cog,self(),runnable)" (but
+                             %% synchronous) is sufficient
+                             task:acquire_token(Cog, Stack),
+                             Ref ! {okthx, self()};
+                         {stop_world, _Sender} ->
+                             %% we already released the token above.  Eat the
+                             %% message or we'll block at inopportune moments
+                             %% later.
+                             Loop();
                          {get_references, Sender} ->
                              ?DEBUG(get_references),
                              Sender ! {gc:extract_references(Stack), self()},
                              Loop()
-                     end end)(),
-            task:acquire_token(Cog, Stack)
+                     end end)()
     end.
 
 poll(Ref) ->
@@ -110,9 +151,9 @@ wait_for_completion(References) ->
         {'EXIT',_,Reason} ->
             gc:unroot_future(self()),
             loop({error,error_transform:transform(Reason)});
-        {completed, Value}->
+        {completed, Result, Sender, SenderCog}->
             gc:unroot_future(self()),
-            loop({ok,Value});
+            convert_to_freestanding_future({ok,Result}, Sender, SenderCog);
         {get_references, Sender} ->
             Sender ! {References, self()},
             wait_for_completion(References);
@@ -121,14 +162,45 @@ wait_for_completion(References) ->
             wait_for_completion(References)
     end.
 
+%% send out notifications to all awaiting processes before allowing
+%% process to terminate.  This avoids spurious "all idle" states which
+%% cause premature clock advances.
+convert_to_freestanding_future(Value, TerminatingProcess, CalleeCog) ->
+    receive
+        {wait,Sender}->
+            %% KLUDGE: we serialize notifications, waiting for one
+            %% okthx before sending out the next ok.  If a large
+            %% number of processes wait on a single future, this will
+            %% make things slower than necessary.
+            Sender ! {ok, self(), CalleeCog},
+            (fun Loop() ->
+                     receive
+                         {okthx,Sender} -> ok;
+                         {get_references, GC} ->
+                             GC ! {gc:extract_references(Value), self()},
+                             Loop()
+                     end
+             end)(),
+            convert_to_freestanding_future(Value, TerminatingProcess, CalleeCog)
+    after 0 ->
+            TerminatingProcess ! {ok, self()},
+            loop(Value)
+    end.
+
+
 %%Servermode
 loop(Value)->
     receive
         {get,Sender} ->
             Sender!{reply,self(),Value},
             loop(Value);
-        {wait,Sender}->
-            Sender!{ok},
+        {wait,Sender} ->
+            Sender!{ok, self()},
+            loop(Value);
+        {okthx, _Sender} ->
+            %% No need to synchronize with "wait" here like in
+            %% convert_to_freestanding_future -- the callee process is already
+            %% gone so we just consume this message.
             loop(Value);
         {get_references, Sender} ->
             ?DEBUG(get_references),
