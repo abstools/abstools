cceb8ade611e41b5c973714ed1001a8e75d60b2d
Volker Stolz <stolz@ifi.uio.no>
2015-09-08 15:54:42 +0200

    Add new ABS.StdLib.Exceptions module to excludes.

--- frontend/src/abs/common/Constants.java
+++ frontend/src/abs/common/Constants.java
@@ -15,9 +15,11 @@ public class Constants {
     public static final String DC_NAME = "ABS.DC";
     public static final String SCHEDULE_NAME = "ABS.Scheduler";
     public static final String PL_NAME = "ABS.Productline";
+    public static final String EXCEPTIONS_NAME = "ABS.StdLib.Exceptions";
     
     public static final List<String> BUILT_IN_LIBS = 
-            Arrays.asList(STDLIB_NAME, FLI_NAME, META_NAME, DC_NAME, SCHEDULE_NAME, PL_NAME);
+            Arrays.asList(STDLIB_NAME, FLI_NAME, META_NAME, DC_NAME, SCHEDULE_NAME, PL_NAME
+                    , EXCEPTIONS_NAME);
     
     public static final String FUNCTIONAL_BREAK_POINT_FUNCTION = "watch";
     public static boolean isFunctionalBreakPointFunctionName(String functionName) {
