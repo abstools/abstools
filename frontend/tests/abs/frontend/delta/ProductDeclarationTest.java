6fc85c5aeed7aab8509a709b951319c23fbd052a
andotri <triando.triando@gmail.com>
2015-11-20 11:04:02 +0700

    Revise unit test

--- frontend/tests/abs/frontend/delta/ProductDeclarationTest.java
+++ frontend/tests/abs/frontend/delta/ProductDeclarationTest.java
@@ -25,7 +25,7 @@ import abs.frontend.typechecker.TypeCheckerHelper;
 public class ProductDeclarationTest extends DeltaTest {
 
     @Test
-    public void featureSet1() {
+    public void oldProductSyntax() {
         Model model = assertParseOk(
                 "product P1(F1, F2, F3);"
                 );
@@ -48,7 +48,7 @@ public class ProductDeclarationTest extends DeltaTest {
     }
 
     @Test
-    public void featureSet2() {
+    public void productFeatureSet() {
         Model model = assertParseOk(
                 "product P1 = {F1, F2, F3};"
                 );
@@ -71,7 +71,7 @@ public class ProductDeclarationTest extends DeltaTest {
     }
 
     @Test
-    public void union1() {
+    public void productUnion() {
         Model model = assertParseOk(
                 "product P1 = {F1, F2, F3} || {F4};"
                 );
@@ -91,35 +91,10 @@ public class ProductDeclarationTest extends DeltaTest {
         for (Feature f : impl.getFeatures())
             actual.add(f.getName());
         assertEquals(expected, actual);
-    }    
-
-    @Test
-    public void union2() {
-        Model model = assertParseOk(
-                "product P1 = {F1, F2, F3};"
-                        + "product P2 = {F4};"
-                        + "product P3 = P1 || P2;"
-                );
-        model.evaluateAllProductDeclarations();
-
-        ProductDecl p = null;
-        try {
-            p = model.findProduct("P3");
-        } catch (WrongProgramArgumentException e) {
-            e.printStackTrace();
-        }
-        ImplicitProduct impl = p.getImplicitProduct();
-        assertEquals(4, impl.getNumFeature());
-
-        Set<String> expected = new HashSet<String>(Arrays.asList("F1", "F2", "F3", "F4"));
-        Set<String> actual = new HashSet<String>();
-        for (Feature f : impl.getFeatures())
-            actual.add(f.getName());
-        assertEquals(expected, actual);
     }
 
     @Test
-    public void intersect1() {
+    public void productIntersect() {
         Model model = assertParseOk(
                 "product P1 = {F1, F2, F3} && {F2, F3};"
                 );
@@ -142,24 +117,23 @@ public class ProductDeclarationTest extends DeltaTest {
     }
 
     @Test
-    public void intersect2() {
+    public void productName() {
         Model model = assertParseOk(
                 "product P1 = {F1, F2, F3};"
-                        + "product P2 = {F2, F3};"
-                        + "product P3 = P1 && P2;"
+                        + "product P2 = P1;"
                 );
         model.evaluateAllProductDeclarations();
 
         ProductDecl p = null;
         try {
-            p = model.findProduct("P3");
+            p = model.findProduct("P2");
         } catch (WrongProgramArgumentException e) {
             e.printStackTrace();
         }
         ImplicitProduct impl = p.getImplicitProduct();
-        assertEquals(2, impl.getNumFeature());
+        assertEquals(3, impl.getNumFeature());
 
-        Set<String> expected = new HashSet<String>(Arrays.asList("F2", "F3"));
+        Set<String> expected = new HashSet<String>(Arrays.asList("F1", "F2", "F3"));
         Set<String> actual = new HashSet<String>();
         for (Feature f : impl.getFeatures())
             actual.add(f.getName());
@@ -167,7 +141,7 @@ public class ProductDeclarationTest extends DeltaTest {
     }
 
     @Test(expected=DeltaModellingException.class)
-    public void cylic() {
+    public void cylicProduct() {
         Model model = assertParseOk(
                 "product P4 = P5;"
                         + "product P5 = P4;"
@@ -177,7 +151,7 @@ public class ProductDeclarationTest extends DeltaTest {
 
     @Test
     public void undeclaredProduct() {
-        Model model = assertParseOk("product P1 = P2 && P3 || P4;");
+        Model model = assertParseOk("product P1 = P2 && P3 || P4 || {F1, F2};");
 
         ProductDecl p = null;
         try {
@@ -194,7 +168,30 @@ public class ProductDeclarationTest extends DeltaTest {
     }
 
     @Test
-    public void parenthesis1(){
+    public void complexExpression() {
+        Model model = assertParseOk(
+                "product P1 = {F1, F2, F3} && {F3, F4} || {F5};"
+                );
+        model.evaluateAllProductDeclarations();
+
+        ProductDecl p = null;
+        try {
+            p = model.findProduct("P1");
+        } catch (WrongProgramArgumentException e) {
+            e.printStackTrace();
+        }
+        ImplicitProduct impl = p.getImplicitProduct();
+        assertEquals(2, impl.getNumFeature());
+
+        Set<String> expected = new HashSet<String>(Arrays.asList("F3", "F5"));
+        Set<String> actual = new HashSet<String>();
+        for (Feature f : impl.getFeatures())
+            actual.add(f.getName());
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void complexExpressionWithParenthesis() {
         Model model = assertParseOk(
                 "product P1 = {F1, F2, F3} && ({F3, F4} || {F5});"
                 );
@@ -217,62 +214,22 @@ public class ProductDeclarationTest extends DeltaTest {
     }
 
     @Test
-    public void parenthesis2(){
-        Model model = assertParseOk(
-                "product P1(F1, F2, F3);"
-                        + "product P2(F3, F4);"
-                        + "product P3(F5);"
-                        + "product P4 = P1 && (P2 || P3);"
-                );
-        model.evaluateAllProductDeclarations();
+    public void rightSideProductName() {
+        Model model = assertParseOk("product P1 = P2 && P3 || P4 || {F1, F2};");
 
         ProductDecl p = null;
         try {
-            p = model.findProduct("P4");
+            p = model.findProduct("P1");
         } catch (WrongProgramArgumentException e) {
             e.printStackTrace();
         }
-        ImplicitProduct impl = p.getImplicitProduct();
-        assertEquals(1, impl.getNumFeature());
 
-        Set<String> expected = new HashSet<String>(Arrays.asList("F3"));
         Set<String> actual = new HashSet<String>();
-        for (Feature f : impl.getFeatures())
-            actual.add(f.getName());
+        p.getProductExpr().setAllProductName(actual);
+        assertEquals(3, actual.size());
+
+        Set<String> expected = new HashSet<String>(Arrays.asList("P2", "P3", "P4"));
         assertEquals(expected, actual);
-    }
-
-    @Test
-    public void complexExpression() {
-        Model model = assertParseOk(
-                "product P1(F1, F2, F3);"
-                        + "product P2(F3, F4);"
-                        + "product P3(F5);"
-                        + "product P4 = P1 && P2;"
-                        + "product P5 = P1 || P2;"
-                        + "product P6 = P1 && P2 || P3 || {F7, F8} || P5;"
-                        + "product P7 = P1 && (P2 || P3) || {F7, F8} || P5;"
-                );
-
-        HashMap<String, Set<String>> products = new HashMap<String, Set<String>>();
-        products.put("P1", new HashSet<String>(Arrays.asList("F1", "F2", "F3")));
-        products.put("P2", new HashSet<String>(Arrays.asList("F3", "F4")));
-        products.put("P3", new HashSet<String>(Arrays.asList("F5")));
-        products.put("P4", new HashSet<String>(Arrays.asList("F3")));
-        products.put("P5", new HashSet<String>(Arrays.asList("F1", "F2", "F3", "F4")));
-        products.put("P6", new HashSet<String>(Arrays.asList("F1", "F2", "F3", "F4", "F5", "F7", "F8")));
-        products.put("P7", new HashSet<String>(Arrays.asList("F1", "F2", "F3", "F4", "F7", "F8")));
-
-        model.evaluateAllProductDeclarations();
-        for (ProductDecl p : model.getProductDecls()) {
-            Set<String> actual = new HashSet<String>();
-            for(Feature f : p.getImplicitProduct().getFeatures()){
-                actual.add(f.getName());
-            }
-            Set<String> expected = products.get(p.getName());
-
-            assertEquals(expected, actual);
-        }
     }
 
 }
