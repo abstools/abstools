4de6664ed53159c7e072c91582dc4ccf109207b6
Rudi Schlatte <rudi@ifi.uio.no>
2016-06-13 14:16:40 +0200

    block_for_resource: add DC argument
    - this makes it possible to consume resources on a different DC than the
      one we’re running at - necessary for bandwidth simulation

--- frontend/src/abs/backend/erlang/runtime/absmodel/src/task.erl
+++ frontend/src/abs/backend/erlang/runtime/absmodel/src/task.erl
@@ -7,7 +7,7 @@
 -export([start/3,init/3,join/1,notifyEnd/1,notifyEnd/2]).
 %%API for tasks
 -export([acquire_token/2,release_token/2,block_with_time_advance/1,block_without_time_advance/1,wait/1,wait_poll/1,commit/1,rollback/1]).
--export([await_duration/4,block_for_duration/4,block_for_resource/4]).
+-export([await_duration/4,block_for_duration/4,block_for_resource/5]).
 -export([loop_for_token/2]).            % low-level; use acquire_token instead
 -export([behaviour_info/1]).
 -include_lib("abs_types.hrl").
@@ -136,7 +136,7 @@ block_for_duration(Cog=#cog{ref=CogRef},Min,Max,Stack) ->
     loop_for_clock_advance(Stack),
     task:acquire_token(Cog, Stack).
 
-block_for_resource(Cog=#cog{ref=CogRef,dc=DC}, Resourcetype, Amount, Stack) ->
+block_for_resource(Cog=#cog{ref=CogRef}, DC, Resourcetype, Amount, Stack) ->
     {Result, Consumed}= dc:consume(DC,Resourcetype,Amount),
     Remaining=rationals:sub(rationals:to_r(Amount), rationals:to_r(Consumed)),
     case Result of
@@ -146,12 +146,12 @@ block_for_resource(Cog=#cog{ref=CogRef,dc=DC}, Resourcetype, Amount, Stack) ->
             task:block_with_time_advance(Cog),           % cause clock advance
             loop_for_clock_advance(Stack),
             task:acquire_token(Cog,Stack),
-            block_for_resource(Cog, Resourcetype, Remaining, Stack);
+            block_for_resource(Cog, DC, Resourcetype, Remaining, Stack);
         ok ->
             case rationals:is_positive(Remaining) of
                 %% We loop since the DC might decide to hand out less
                 %% than we ask for and less than it has available.
-                true -> block_for_resource(Cog, Resourcetype, Remaining, Stack);
+                true -> block_for_resource(Cog, DC, Resourcetype, Remaining, Stack);
                 false -> ok
             end
     end.
