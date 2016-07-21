ad6efd911925c5471301c4746d91f44dd95c3c8b
Volker Stolz <stolz@ifi.uio.no>
2016-07-21 12:36:54 +0200

    Add test for bug #197.

--- frontend/tests/abs/frontend/typesystem/NegativeTypeCheckerTests.java
+++ frontend/tests/abs/frontend/typesystem/NegativeTypeCheckerTests.java
@@ -312,12 +312,16 @@ public class NegativeTypeCheckerTests extends FrontendTest {
     @Test
     public void caseBoundVarWrongType() {
         assertTypeErrors("def Bool f(Bool x) = let (Int y) = 5 in case x { y => True; };");
-
     }
 
     @Test
     public void caseErrorConstructorNotResolvable() {
         assertTypeErrors("{ Bool x = True; Bool b = case x { Foo => False; }; }");
+    }
+
+    @Test
+    public void defTC1_ticket197() {
+        assertTypeErrors("def A f<A,A,A,A,A,A,A,A,A,A>(A a) = a;");
     }
 
     @Test
