abstools/abstools

commit db13b084d343ffc37455b744fd6dbb65f1e8e150

Let GC know when object is dying for other reasons

frontend/src/abs/backend/erlang/runtime/absmodel/src/object.erl
@@ -218,7 +218,7 @@ handle_sync_event({die,Reason,By},_From,_StateName,S=#state{class=C, cog=Cog, ta
                 true -> exit(By,Reason);
                 false -> ok
             end,
-            {stop,normal,ok,S#state{alive=false}}
+            {stop,{shutdown, Reason},ok,S#state{alive=false}}
     end;
 handle_sync_event(protect_from_gc, _From, StateName, S) ->
     {reply, ok, StateName, S#state{protect_from_gc=true}};
@@ -238,7 +238,7 @@ handle_event(unprotect_from_gc, StateName, State=#state{class=C,tasks=Tasks,cog=
             %% FIXME: check if cog_monitor needs updating here wrt task lists etc.
             [ exit(T,normal) ||T<-gb_sets:to_list(Tasks)],
             cog:dec_ref_count(Cog),
-            {stop,normal,ok,State}
+            {stop,{shutdown, unprotected},ok,State}
     end;
 handle_event(_Event,_StateName,State)->
     {stop,not_implemented,State}.
@@ -248,8 +248,10 @@ handle_info({'DOWN', _MonRef, process, TaskRef, _Reason} ,StateName,S=#state{tas
     {next_state,StateName,S#state{tasks=gb_sets:del_element(TaskRef, Tasks)}}.
 
 
-
-terminate(_Reason,_StateName,_Data)->
+terminate({shutdown, gc}, _StateName, _Data) ->
+    ok;
+terminate({shutdown, _Reason},_StateName,_Data)->
+    gc:unregister_object(self()),
     ok.
 code_change(_OldVsn,_StateName,_Data,_Extra)->
     not_implemented.

frontend/src/abs/backend/erlang/runtime/absmodel/src/gc.erl
@@ -8,7 +8,7 @@
 -export([start/2, stop/0, extract_references/1, get_references/1]).
 -export([register_future/1, unroot_future/1]).
 -export([register_cog/1, unregister_cog/1, cog_stopped/1]).
--export([register_object/1]).
+-export([register_object/1,unregister_object/1]).
 
 -export([behaviour_info/1]).
 
@@ -82,6 +82,11 @@ register_object(Obj) ->
     %% Obj is an #object record
     gen_fsm:send_event({global, gc}, Obj).
 
+unregister_object(#object{ref=Obj}) ->
+    gen_fsm:send_event({global, gc}, {stopped_object, Obj});
+unregister_object(Obj) when is_pid(Obj) ->
+    gen_fsm:send_event({global, gc}, {stopped_object, Obj}).
+
 
 %% gen_fsm callback functions
 
@@ -121,6 +126,9 @@ idle_state_next(State=#state{log=Log, cogs=Cogs}) ->
 idle(#object{ref=Ref}, State=#state{objects=Objects}) ->
     {Next, NewState} = idle_state_next(State#state{objects=gb_sets:insert({object, Ref}, Objects)}),
     {next_state, Next, NewState};
+idle({stopped_object, Ref}, State=#state{objects=Objects}) ->
+    {Next, NewState} = idle_state_next(State#state{objects=gb_sets:del_element({object, Ref}, Objects)}),
+    {next_state, Next, NewState};
 idle({unroot, Sender}, State=#state{root_futures=RootFutures, futures=Futures}) ->
     {Next, NewState} = idle_state_next(State#state{futures=gb_sets:insert({future, Sender}, Futures),
                                                    root_futures=gb_sets:delete({future, Sender}, RootFutures)}),
@@ -159,6 +167,9 @@ collecting_state_next(State=#state{cogs_waiting_to_stop=RunningCogs, cogs=Cogs, 
 
 collecting(#object{ref=Ref}, State=#state{objects=Objects}) ->
     {Next, NewState} = collecting_state_next(State#state{objects=gb_sets:insert({object, Ref}, Objects)}),
+    {next_state, Next, NewState};
+collecting({stopped_object, Ref}, State=#state{objects=Objects}) ->
+    {Next, NewState} = collecting_state_next(State#state{objects=gb_sets:del_element({object, Ref}, Objects)}),
     {next_state, Next, NewState};
 collecting({unroot, Sender}, State=#state{root_futures=RootFutures, futures=Futures}) ->
     {Next, NewState} = collecting_state_next(State#state{futures=gb_sets:insert({future, Sender}, Futures),

commit 4119d1f40ff14d0fb1d5275ab9a66e6889abd37a

Handle gen_fsm object shutdown properly
- shutdown the fsm with reason `normal' always, since otherwise the
  whole system terminates, even though the documentation indicates that
  `{shutdown, term()}' is not logged either.

- Introduce `termination_reason' in state record so we know whether to
  unregister from gc or not.

- Is a supervisor shutting down the whole system?  TODO: read up on
  supervision trees; maybe do not start objects with
  `gen_fsm:start_link'.

- Bug introduced in db13b084d343ffc37455b744fd6dbb65f1e8e150

frontend/src/abs/backend/erlang/runtime/absmodel/src/object.erl
@@ -117,7 +117,8 @@ await_activation(Params) ->
                class,                     % The class of the object (a symbol)
                fields,                    % the state of the object fields
                alive=true,     % false if object is garbage collected / crashed
-               protect_from_gc=false % true if unreferenced object needs to  be kept alive
+               protect_from_gc=false, % true if unreferenced object needs to  be kept alive
+               termination_reason     % communicate to terminate() function
               }).
 
 start(Cog,Class)->
@@ -218,7 +219,7 @@ handle_sync_event({die,Reason,By},_From,_StateName,S=#state{class=C, cog=Cog, ta
                 true -> exit(By,Reason);
                 false -> ok
             end,
-            {stop,{shutdown, Reason},ok,S#state{alive=false}}
+            {stop,normal,ok,S#state{alive=false,termination_reason=Reason}}
     end;
 handle_sync_event(protect_from_gc, _From, StateName, S) ->
     {reply, ok, StateName, S#state{protect_from_gc=true}};
@@ -238,7 +239,7 @@ handle_event(unprotect_from_gc, StateName, State=#state{class=C,tasks=Tasks,cog=
             %% FIXME: check if cog_monitor needs updating here wrt task lists etc.
             [ exit(T,normal) ||T<-gb_sets:to_list(Tasks)],
             cog:dec_ref_count(Cog),
-            {stop,{shutdown, unprotected},ok,State}
+            {stop,normal,ok,State#state{termination_reason=gc}}
     end;
 handle_event(_Event,_StateName,State)->
     {stop,not_implemented,State}.
@@ -247,10 +248,9 @@ handle_event(_Event,_StateName,State)->
 handle_info({'DOWN', _MonRef, process, TaskRef, _Reason} ,StateName,S=#state{tasks=Tasks})->
     {next_state,StateName,S#state{tasks=gb_sets:del_element(TaskRef, Tasks)}}.
 
-
-terminate({shutdown, gc}, _StateName, _Data) ->
+terminate(normal, _StateName, _Data=#state{termination_reason=gc}) ->
     ok;
-terminate({shutdown, _Reason},_StateName,_Data)->
+terminate(_Reason,_StateName, _Data)->
     gc:unregister_object(self()),
     ok.
 code_change(_OldVsn,_StateName,_Data,_Extra)->