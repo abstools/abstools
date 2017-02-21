84a4eaaa01586196062e9d8660dfc0b54532f655
Radu Muschevici <radu@cs.tu-darmstadt.de>
2017-02-21 16:10:53 +0100

    Java code generation fix

--- frontend/src/abs/backend/java/codegeneration/JavaCodeStream.java
+++ frontend/src/abs/backend/java/codegeneration/JavaCodeStream.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2009-2011, The HATS Consortium. All rights reserved. 
+ * Copyright (c) 2009-2011, The HATS Consortium. All rights reserved.
  * This file is licensed under the terms of the Modified BSD License.
  */
 package abs.backend.java.codegeneration;
@@ -10,20 +10,16 @@ import java.io.OutputStream;
 import java.io.PrintStream;
 import java.io.UnsupportedEncodingException;
 
+import com.google.common.base.Strings;
+
 import abs.backend.java.JavaBackend;
 
 public class JavaCodeStream extends PrintStream {
     private static final int INDENT_LENGTH = 4;
-    private static final String INDENT1;
-    static {
-        String s = "";
-        for (int i=0; i < INDENT_LENGTH; i++)
-            s += " ";
-        INDENT1 = s;
-    }
+    private static final String INDENT1 = Strings.repeat(" ", INDENT_LENGTH);
 
     private Boolean startNewLine = true;
-    private String indent = ""; 
+    private String indent = "";
 
     public JavaCodeStream(OutputStream out) throws UnsupportedEncodingException {
         super(out, false, JavaBackend.CHARSET);
@@ -36,12 +32,12 @@ public class JavaCodeStream extends PrintStream {
     public void incIndent() {
         indent = indent + INDENT1;
     }
-    
+
     public void decIndent() {
         if (indent.length() >= INDENT_LENGTH)
             indent = indent.substring(0, indent.length() - INDENT_LENGTH);
     }
-    
+
     /*
      * print statement that tries to be smart about code indentation:
      * if the previous print statement ended with a line separator,
@@ -57,17 +53,17 @@ public class JavaCodeStream extends PrintStream {
                 decIndent();
             super.print(indent);
         }
-            
-        
+
+
         super.print(s);
         startNewLine = false;
     }
-    
+
     /*
      * println statement that tries to be smart about code indentation:
      * If string ends with "{", it will increase the indentation,
-     * if string is equal to "}", it will decrease indentation. 
-     * 
+     * if string is equal to "}", it will decrease indentation.
+     *
      * This to work requires some collaboration from the programmer:
      * - open braces should be the last character in a line of code, i.e. println("... {")
      * - closing braces should be the first character in a line of code, i.e. println("} ...")
@@ -76,16 +72,16 @@ public class JavaCodeStream extends PrintStream {
      */
     @Override
     public void println(String s) {
-       //System.err.println("*** " + indent.length() + " *** " + s + " ***");
+        //System.err.println("*** " + indent.length() + " *** " + s + " ***");
         if (s.startsWith("}") || s.equals(")"))
             decIndent();
 
-        if (startNewLine) 
+        if (startNewLine)
             super.print(indent);
         super.print(s);
         super.println();
         startNewLine = true;
-        
+
         if (s.endsWith("{") || s.endsWith("("))
             incIndent();
     }
