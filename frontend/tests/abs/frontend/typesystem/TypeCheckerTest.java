c7c79c9eba5a4c6173dab082d70fd69785bee311
Rudi Schlatte <rudi@ifi.uio.no>
2017-06-19 17:13:40 +0200

    handle mixed-but-upgradable types in list literals

--- frontend/tests/abs/frontend/typesystem/TypeCheckerTest.java
+++ frontend/tests/abs/frontend/typesystem/TypeCheckerTest.java
@@ -322,6 +322,11 @@ public class TypeCheckerTest extends FrontendTest {
     }
 
     @Test
+    public void fnAppListUpgradableListType() {
+        assertTypeOK("{ Set<Rat> s = set[4/3, 4]; }");
+    }
+
+    @Test
     public void constructorTypeArgs() {
         assertTypeOK("{ Maybe<Bool> o = Just(True); }");
     }
