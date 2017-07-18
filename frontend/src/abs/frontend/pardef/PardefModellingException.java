380e4423c16dcfb8f57b85b2653e56692887f9f4
Björn Petersen <pheasn@gmail.com>
2017-07-18 15:34:39 +0200

    Replace DeltaModellingException usage with PardefModellingException

---
+++ frontend/src/abs/frontend/pardef/PardefModellingException.java
@@ -0,0 +1,28 @@
+package abs.frontend.pardef;
+
+import abs.frontend.ast.ASTNode;
+
+public final class PardefModellingException extends RuntimeException {
+
+    public PardefModellingException() {
+        super();
+    }
+
+    public PardefModellingException(String message) {
+        super(message);
+    }
+
+    public PardefModellingException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public PardefModellingException(ASTNode<?> node, String message) {
+        // TODO add info about node
+        super(message);
+    }
+
+    public PardefModellingException(ASTNode<?> node, String message, Throwable cause) {
+        // TODO add info about node
+        super(message, cause);
+    }
+}
