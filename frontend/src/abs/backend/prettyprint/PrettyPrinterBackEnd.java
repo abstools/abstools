d56561e3dae158172a06ea72e2559e44080e7b94
Rudi Schlatte <rudi@constantly.at>
2017-07-23 09:51:11 +0200

    allow pretty printer to keep sugared statements
    - keeps statements like "await o!m()" instead of rewriting them into their
      basic core abs equivalents
    
    - off by default, activated with ‘-keepsugar’ command-line switch

--- frontend/src/abs/backend/prettyprint/PrettyPrinterBackEnd.java
+++ frontend/src/abs/backend/prettyprint/PrettyPrinterBackEnd.java
@@ -18,6 +18,7 @@ import abs.frontend.tests.ABSFormatter;
 public class PrettyPrinterBackEnd extends Main {
     private File outputfile;
     private boolean force = false;
+    private boolean keepsugar = false;
     
     public static void main(final String... args) {
         PrettyPrinterBackEnd backEnd = new PrettyPrinterBackEnd();
@@ -57,6 +58,8 @@ public class PrettyPrinterBackEnd extends Main {
                 }
             } else if (arg.equals("-f"))  {
                 force = true;
+            } else if (arg.equals("-keepsugar"))  {
+                keepsugar = true;
             } else if (arg.equals("-prettyprint")) {
                 // nothing to do
             } else {
@@ -72,7 +75,11 @@ public class PrettyPrinterBackEnd extends Main {
      * @throws Exception
      */
     public void compile(String[] args) throws Exception {
-        final Model model = parse(args);
+        final Model model = parseFiles(parseArgs(args).toArray(new String[0]));
+        if (keepsugar) {
+            model.doAACrewrite = false;
+        }
+        analyzeModel(model);
         if (! force && (model.hasParserErrors() || model.hasErrors() || model.hasTypeErrors())) {
             printParserErrorAndExit();
         }
@@ -99,6 +106,7 @@ public class PrettyPrinterBackEnd extends Main {
     public static void printUsage() {
         System.out.println("ABS Pretty Printer (-prettyprint):\n"
                 + "  -f             force pretty printing even if there are type errors\n"
+                + "  -keepsugar     do not transform statements into basic core abs\n"
                 + "  -o <file>      write output to <file> instead of standard output\n"
         );
     }
