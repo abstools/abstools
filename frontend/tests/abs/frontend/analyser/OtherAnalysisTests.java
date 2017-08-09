8e2d7097e3b520660f5dae2f157b9647f5ca671b
Rudi Schlatte <rudi@ifi.uio.no>
2017-08-09 15:34:29 +0200

    fix unit test
    - do not try to work on tree fragments; rewrite rules will crash trying to
      find the parent
    
    - note semantic errors in some unrelated tests

--- frontend/tests/abs/frontend/analyser/OtherAnalysisTests.java
+++ frontend/tests/abs/frontend/analyser/OtherAnalysisTests.java
@@ -116,11 +116,13 @@ public class OtherAnalysisTests extends FrontendTest {
     //@Test
     public void awaitTest2() {
         Model.doAACrewrite = true;
+        // FIXME: the code in this example is incorrect (no await statement in init block allowed)
         Model m = assertParseOk("data Unit; interface I { Unit m(Unit x); } class C implements I {{Unit x = await this!m(Unit());}}");
         assertFalse(m.hasErrors());
         final String p1 = prettyPrint(m);
         Model.doAACrewrite = false;
         // Model m2 = assertParseOk("data Unit; interface I { Unit m(); } class C implements I {{Unit x = await this!m();}}");
+        // FIXME: the code in this example is incorrect (no await statement in init block allowed)
         Model m2 = assertParseOk("data Unit; interface I { Unit m(Unit x); } class C implements I {{Unit x = await this!m(Unit());}}");
         assertFalse(m2.hasErrors());
         assertEquals(p1, prettyPrint(m2));
@@ -129,10 +131,12 @@ public class OtherAnalysisTests extends FrontendTest {
     //@Test
     public void awaitTest3() {
         Model.doAACrewrite = true;
+        // FIXME: the code in this example is incorrect (no await statement in init block allowed)
         Model m = assertParseOk("data Unit; interface I { Unit m(Unit x); } class C implements I {{Unit x = await this!n(Unit());}}");
         assertFalse(m.hasErrors());
         final String p1 = prettyPrint(m);
         Model.doAACrewrite = false;
+        // FIXME: the code in this example is incorrect (no await statement in init block allowed)
         Model m2 = assertParseOk("data Unit; interface I { Unit m(Unit x); } class C implements I {{Unit x = await this!n(Unit());}}");
         assertFalse(m2.hasErrors());
         assertEquals(p1, prettyPrint(m2));
@@ -140,6 +144,7 @@ public class OtherAnalysisTests extends FrontendTest {
 
     @Test
     public void testContext1() {
+        // FIXME: the code in this example is incorrect (no await statement in init block allowed)
         Model m = assertParseOk("data Unit; interface I { Unit m(); } class C implements I {{Unit x = await this!m();}}");
         ClassDecl cd = (ClassDecl) m.lookupModule("UnitTest").getDecl(2);
         AwaitAsyncCall n = (AwaitAsyncCall) down(cd);
@@ -148,6 +153,7 @@ public class OtherAnalysisTests extends FrontendTest {
 
     @Test
     public void testContext2() {
+        // FIXME: the code in this example is incorrect (no await statement in init block allowed)
         Model m = assertParseOk("data Unit; interface I { Unit m(); } class C implements I {{Unit x = await this!m();}}");
         ClassDecl cd = (ClassDecl) m.lookupModule("UnitTest").getDecl(2);
         AwaitAsyncCall n = (AwaitAsyncCall) down(cd);
@@ -204,9 +210,8 @@ public class OtherAnalysisTests extends FrontendTest {
     @Test
     public void awaitRewriteDecl2() throws Exception {
         String deltaDecl = "delta D; modifies class C { adds Unit m() { return await this!m();}}";
-        CompilationUnit u = new ABSParserWrapper(null, true, false)
-            .parse(new StringReader(deltaDecl));
-        DeltaDecl d = u.getDeltaDecl(0);
+        Model m = assertParseOk(deltaDecl);
+        DeltaDecl d = m.findDelta("D");
         AwaitAsyncCall a = (AwaitAsyncCall) down(d);
         assertNotNull(a); // pity, would like this to work.
     }
