5d5ca5ceba247d073b075e99a54bb5e11e03f9b7
Rudi Schlatte <rudi@ifi.uio.no>
2018-03-23 13:07:34 +0100

    skip failing exception tests
    - I'd love to have a way of skipping tests depending on the backend using
      annotations or categories, but using Assume will do for now.

--- frontend/tests/abs/backend/common/ExceptionTests.java
+++ frontend/tests/abs/backend/common/ExceptionTests.java
@@ -6,11 +6,13 @@ package abs.backend.common;
 
 import java.io.File;
 
+import org.junit.Assume;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.Parameterized;
 
 import abs.backend.BackendTestDriver;
+import abs.backend.BackendTestDriver.BackendName;
 
 @RunWith(Parameterized.class)
 public class ExceptionTests extends SemanticTests {
@@ -20,31 +22,37 @@ public class ExceptionTests extends SemanticTests {
         
    @Test
    public void ticket175() {
+       Assume.assumeFalse("Not implemented on Java backend yet", driver.getBackendName() == BackendName.JAVA);
        assertEvalTrue(new File("tests/abssamples/backend/StmtTests/exception_ticket175.abs"));
    }
  
    @Test
    public void testException() {
+       Assume.assumeFalse("Not implemented on Java backend yet", driver.getBackendName() == BackendName.JAVA);
        assertEvalTrue("exception MyE(Bool); { Bool testresult = False; try throw MyE(True); catch MyE(value) => testresult = value; }");
    }
    
    @Test
    public void divByZero() throws Exception {
+       Assume.assumeFalse("Not implemented on Maude backend yet", driver.getBackendName() == BackendName.MAUDE);
        assertEvalFails("{Bool testresult = 1/0 != 0;}");
    }
 
    @Test
    public void divByZeroCaught() throws Exception {
+       Assume.assumeFalse("Not implemented on Java / Maude backend yet", driver.getBackendName() == BackendName.JAVA || driver.getBackendName() == BackendName.MAUDE);
        assertEvalTrue("{Bool testresult = False; try 1/0; catch DivisionByZeroException => testresult = True; }");
    }
 
    @Test
    public void testExceptionPropagation() {
+       Assume.assumeFalse("Not implemented on Java backend yet", driver.getBackendName() == BackendName.JAVA);
        assertEvalTrue(new File("tests/abssamples/backend/StmtTests/exception_propagation.abs"));
    }
    
    @Test
    public void testExceptionNullFuture() {
+       Assume.assumeFalse("Not implemented on Java / Maude backend yet", driver.getBackendName() == BackendName.JAVA || driver.getBackendName() == BackendName.MAUDE);
        assertEvalTrue(new File("tests/abssamples/backend/StmtTests/exception_nullfuture.abs"));
    }
 }
