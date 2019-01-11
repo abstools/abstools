83949e97d0d8db5612f9690404c5217f7dbc7bb1
Rudi Schlatte <rudi@constantly.at>
2019-01-11 17:35:57 +0700

    slight refactoring of pretty-printer tests
    - do not desugar (i.e., keep `foreach` and `await o!m` statements)
    
    - add reading of sample files
    
    - add `assertEqualsNoWS` to compare string and resulting pretty-printed model

--- frontend/src/test/java/org/abs_models/backend/prettyprint/PrettyPrinterTests.java
+++ frontend/src/test/java/org/abs_models/backend/prettyprint/PrettyPrinterTests.java
@@ -6,16 +6,19 @@ package org.abs_models.backend.prettyprint;
 
 import static org.junit.Assert.assertEquals;
 
+import java.io.IOException;
 import java.io.PrintWriter;
 import java.io.StringReader;
 import java.io.StringWriter;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 import org.abs_models.ABSTest;
 import org.abs_models.frontend.ast.ASTNode;
 import org.abs_models.frontend.ast.DeltaDecl;
 import org.abs_models.frontend.ast.Model;
 import org.abs_models.frontend.parser.Main;
-import org.abs_models.frontend.tests.ABSFormatter;
 import org.junit.Test;
 
 public class PrettyPrinterTests extends ABSTest {
@@ -25,39 +28,71 @@ public class PrettyPrinterTests extends ABSTest {
         String deltaDecl = "delta Foo;adds data States=F|B|I|M;";
         DeltaDecl d = (DeltaDecl) Main
             .parseUnit(null, new StringReader(deltaDecl), true).getDeltaDecl(0);
-        assertEquals("deltaFoo;addsdataStates=F|B|I|M;", replaceWhitespaceChars(prettyPrint(d)));
+        assertEqualsNoWS(deltaDecl, d);
     }
 
     @Test
     public void prettyPrinterModifyInterfaceModifierTest() throws Exception{
         String deltaDecl = "delta Foo;modifies interface X{removes Int fooMethod();adds Int fooMethod();}";
-        Model m = assertParse(deltaDecl, Config.WITHOUT_MODULE_NAME);
-        assertEquals("deltaFoo;modifiesinterfaceX{removesIntfooMethod();addsIntfooMethod();}", replaceWhitespaceChars(prettyPrint(m)));
+        Model m = assertParse(deltaDecl, Config.WITHOUT_MODULE_NAME, Config.WITHOUT_DESUGARING);
+        assertEqualsNoWS(deltaDecl, m);
     }
     
     @Test
     public void prettyPrinterListLiteralTest() throws Exception {
         String ms = "module Test; { List<Int> x = list[1, 2, 3]; }";
-        Model m = assertParse(ms, Config.WITHOUT_MODULE_NAME);
-        assertEquals("moduleTest;{List<Int>x=list[1,2,3];}", replaceWhitespaceChars(prettyPrint(m)));
+        Model m = assertParse(ms, Config.WITHOUT_MODULE_NAME, Config.WITHOUT_DESUGARING);
+        assertEqualsNoWS(ms, m);
     }
 
     @Test
     public void prettyPrinterFloatLiteralTest() throws Exception {
         String ms = "module Test; { Float x = 3.1415927; }";
-        Model m = assertParse(ms, Config.WITHOUT_MODULE_NAME);
-        assertEquals("moduleTest;{Floatx=3.1415927;}", replaceWhitespaceChars(prettyPrint(m)));
+        Model m = assertParse(ms, Config.WITHOUT_MODULE_NAME, Config.WITHOUT_DESUGARING);
+        assertEqualsNoWS(ms, m);
     }
 
-    private String prettyPrint(ASTNode<?> d) {
+    @Test
+    public void prettyPrinterLiterals() throws Exception {
+        String ms = readFile("abssamples/backend/PrettyPrinterTests/Literals.abs");
+        Model m = assertParse(ms, Config.WITHOUT_MODULE_NAME, Config.WITHOUT_DESUGARING);
+        assertEqualsNoWS(ms, m);
+    }
+
+    @Test
+    public void prettyPrinterExpressions() throws Exception {
+        // TODO: run with -keepsugar
+        // Untested:
+        // - ParFnApp
+        // - AwaitAsyncCall
+        // - OriginalCall
+        String ms = readFile("abssamples/backend/PrettyPrinterTests/PureExpressions.abs");
+        Model m = assertParse(ms, Config.WITHOUT_MODULE_NAME, Config.WITHOUT_DESUGARING);
+        assertEqualsNoWS(ms, m);
+        ms = readFile("abssamples/backend/PrettyPrinterTests/EffExpressions.abs");
+        m = assertParse(ms, Config.WITHOUT_MODULE_NAME, Config.WITHOUT_DESUGARING);
+        assertEqualsNoWS(ms, m);
+    }
+
+    private static void assertEqualsNoWS(String s, ASTNode<?> m) {
+        assertEquals(replaceWhitespaceChars(s), replaceWhitespaceChars(prettyPrint(m)));
+    }
+
+    private static String readFile(String filename) throws IOException
+    {
+        byte[] encoded = Files.readAllBytes(Paths.get(resolveFileName(filename)));
+        return new String(encoded, StandardCharsets.UTF_8);
+    }
+
+    private static String prettyPrint(ASTNode<?> d) {
         StringWriter writer = new StringWriter();
         PrintWriter w = new PrintWriter(writer);
-        ABSFormatter f = new DefaultABSFormatter(w);
+        org.abs_models.frontend.tests.ABSFormatter f = new DefaultABSFormatter(w);
         d.doPrettyPrint(w,f);
         return writer.toString();
     }
 
-    private String replaceWhitespaceChars(String in){
+    private static String replaceWhitespaceChars(String in){
         return in.replace("\n", "").replace("\r", "").replace(" ", "");
     }
 
