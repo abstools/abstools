b80cc9f7bfbb99a1e543a881a496a992ac5335a8
Rudi Schlatte <rudi@ifi.uio.no>
2019-03-04 11:26:01 +0100

    simplify, comment in shell scripts

--- frontend/src/main/resources/win/debugJava.bat
+++ frontend/src/main/resources/win/debugJava.bat
@@ -1,5 +1,5 @@
 @echo off
-
+rem NOTE: compile with `absc java --debuginfo ...` for this to work
 set BASEDIR=%~dp0..\..\
 
 java -cp %BASEDIR%bin;%~1 -Dabs.debug=true -Dabs.loglevel=finest -Dabs.systemobserver=GraphicalDebugger -Dabs.totalscheduler=InteractiveScheduler %~2.Main
