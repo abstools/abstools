18687c3961df6138ba21c850a0790994160c5e63
Rudi Schlatte <rudi@constantly.at>
2019-07-23 15:13:39 +0200

    move creation of default case branch from parser to preprocessor

--- frontend/src/main/java/org/abs_models/frontend/antlr/parser/CreateJastAddASTListener.java
+++ frontend/src/main/java/org/abs_models/frontend/antlr/parser/CreateJastAddASTListener.java
@@ -455,25 +455,7 @@ public class CreateJastAddASTListener extends ABSBaseListener {
         setV(ctx, new ExpressionStmt(v(ctx.annotations()), v(ctx.exp())));
     }
     @Override public void exitCaseStmt(ABSParser.CaseStmtContext ctx) {
-        List<CaseBranchStmt> branches = l(ctx.casestmtbranch());
-        CaseBranchStmt lastbranch = null;
-        if (branches.getNumChildNoTransform() > 0) {
-            lastbranch = branches.getChildNoTransform(branches.getNumChildNoTransform() - 1);
-        }
-        if (lastbranch == null || !(lastbranch.getLeftNoTransform() instanceof UnderscorePattern)) {
-            // Add default branch that throws
-            // PatternMatchFailException.  See "Behavior of
-            // non-exhaustive case statement: no branch match = skip
-            // or error?" on abs-dev on Jan 25-26, 2017
-            Block block = new Block(new List<>(), new List<>());
-            block.addStmt(new ThrowStmt(new List<>(),
-                                        new DataConstructorExp("PatternMatchFailException",
-                                                               new List<>())));
-            CaseBranchStmt defaultBranch = new CaseBranchStmt(new UnderscorePattern(), block);
-            setASTNodePosition(ctx, defaultBranch);
-            branches.add(defaultBranch);
-        }
-        setV(ctx, new CaseStmt(v(ctx.annotations()), v(ctx.c), branches));
+        setV(ctx, new CaseStmt(v(ctx.annotations()), v(ctx.c), l(ctx.casestmtbranch())));
     }
     @Override public void exitCasestmtbranch(ABSParser.CasestmtbranchContext ctx) {
         Stmt body = v(ctx.stmt());
