ba8143a200d837b36032a89cc54b1889deeef79c
Rudi Schlatte <rudi@ifi.uio.no>
2020-02-24 18:24:16 +0100

    Implement transitive module imports/exports (pt.2)
    - Implement import/export of specific names instead of ‘*’.

--- org.abs_models.xtext/src/main/java/org/abs_models/xtext/scoping/AbsImportedNamespaceAwareLocalScopeProvider.java
+++ org.abs_models.xtext/src/main/java/org/abs_models/xtext/scoping/AbsImportedNamespaceAwareLocalScopeProvider.java
@@ -37,10 +37,9 @@ public class AbsImportedNamespaceAwareLocalScopeProvider extends ImportedNamespa
     @Inject
     private ResourceDescriptionsProvider rdp;
 
-
     // We override `internalGetImportedNamespaceResolvers` instead of
     // `getImportedNamespaceResolvers` because we do want to cache the result
-    // and the `cache` instance variable is private.
+    // and the `cache` instance variable of the superclass is private.
     //
     // We override this method instead of `getImportedNamespace` or
     // `createImportedNamespaceResolver` because `import` statements can
@@ -58,15 +57,15 @@ public class AbsImportedNamespaceAwareLocalScopeProvider extends ImportedNamespa
             final ModuleDeclaration moduleDecl = (ModuleDeclaration) context;
             // Find all module declarations -- this happens before linking so
             // we have to do it manually
-            Map<String, ModuleDeclaration> modules = new HashMap<>();
-            IResourceDescriptions index = rdp.getResourceDescriptions(moduleDecl.eResource());
-            for (IEObjectDescription d : index.getExportedObjectsByType(moduleDecl.eClass())) {
+            final Map<String, ModuleDeclaration> modules = new HashMap<>();
+            final IResourceDescriptions index = rdp.getResourceDescriptions(moduleDecl.eResource());
+            for (final IEObjectDescription d : index.getExportedObjectsByType(moduleDecl.eClass())) {
                 modules.put(d.getQualifiedName().toString(), (ModuleDeclaration) d.getEObjectOrProxy());
             }
             // Handle implicit import of standard library.  We do not override
             // the `getImplicitImports` method since we only import all of
             // ABS.StdLib if no part of it has been explicitly imported.
-            boolean hasStdLibImport = moduleDecl.getImports().stream().anyMatch(i -> i.getModulename().equals("ABS.StdLib"));
+            final boolean hasStdLibImport = moduleDecl.getImports().stream().anyMatch(i -> i.getModulename().equals("ABS.StdLib"));
             if (!hasStdLibImport) {
                 final ImportNormalizer resolver = createImportedNamespaceResolver("ABS.StdLib.*", ignoreCase);
                 if (resolver != null) importedNamespaceResolvers.add(resolver);
@@ -121,17 +120,21 @@ public class AbsImportedNamespaceAwareLocalScopeProvider extends ImportedNamespa
      * @param moduleName the name of the module we’re importing from
      * @param ignoreCase whether to ignore case
      */
-    private void importAllFromModule(List<ImportNormalizer> resolvers, String moduleName, Map<String, ModuleDeclaration> modules, boolean ignoreCase) {
+    private void importAllFromModule(final List<ImportNormalizer> resolvers,
+                                     final String moduleName,
+                                     final Map<String, ModuleDeclaration> modules,
+                                     final boolean ignoreCase)
+    {
         // Give up early if module not found -- will be caught during
         // validation
         if (!modules.containsKey(moduleName)) return;
-        ModuleDeclaration module = modules.get(moduleName);
+        final ModuleDeclaration module = modules.get(moduleName);
         if (module == null) return;
-        for (ModuleExport export : module.getExports()) {
+        for (final ModuleExport export : module.getExports()) {
             if (export.getModulename() != null) {
                 if (export.isStar()) {
                     // export * from Mod;
-                    for (ModuleImport imp : module.getImports()) {
+                    for (final ModuleImport imp : module.getImports()) {
                         if (export.getModulename().equals(imp.getModulename())) {
                             if (imp.isStar()) {
                                 // export * from Mod; + import * from Mod;
@@ -145,19 +148,17 @@ public class AbsImportedNamespaceAwareLocalScopeProvider extends ImportedNamespa
                 } else {
                     // export A, b, C from Mod;
                     // TODO write validator that checks for import of at least A, b, C
-                    for (ModuleImport imp : module.getImports()) {
+                    for (final ModuleImport imp : module.getImports()) {
                         if (export.getModulename().equals(imp.getModulename())) {
                             if (imp.isStar()) {
                                 // export A, b, C from Mod; + import * from Mod;
                                 importNamesFromModule(resolvers, imp.getModulename(), export.getIdentifiers(), modules, ignoreCase);
                             } else {
                                 // export A, b, C from Mod; + import A, e, F from Mod;
-
-                                // KLUDGE: this set operation is expensive,
-                                // but the number of inputs is small
-                                importNamesFromModule(resolvers, imp.getModulename(),
-                                                      export.getIdentifiers().stream().filter(s -> imp.getIdentifiers().contains(s))
-                                                      .collect(Collectors.toList()), modules, ignoreCase);
+                                importNamesFromModule(resolvers,
+                                                      imp.getModulename(),
+                                                      intersect_ids(export.getIdentifiers(), imp.getIdentifiers()),
+                                                      modules, ignoreCase);
                             }
                         }
                     }
@@ -169,7 +170,7 @@ public class AbsImportedNamespaceAwareLocalScopeProvider extends ImportedNamespa
                     if (resolver != null) resolvers.add(resolver);
                 } else {
                     // export A, b, C;
-                    for (String id : export.getIdentifiers()) {
+                    for (final String id : export.getIdentifiers()) {
                         final ImportNormalizer resolver = createImportedNamespaceResolver(moduleName + "." + id, ignoreCase);
                         if (resolver != null) resolvers.add(resolver);
                     }
@@ -188,11 +189,91 @@ public class AbsImportedNamespaceAwareLocalScopeProvider extends ImportedNamespa
      * @param simpleNames the list of names to be imported.
      * @param ignoreCase whether to ignore case
      */
-    private void importNamesFromModule(List<ImportNormalizer> resolvers, String moduleName, List<String> simpleNames, Map<String, ModuleDeclaration> modules, boolean ignoreCase) {
-        // TODO: find module, import intersection of simpleNames and exported names
-        for (final String id : simpleNames) {
-            final ImportNormalizer resolver = createImportedNamespaceResolver(moduleName + "." + id, ignoreCase);
-            if (resolver != null) resolvers.add(resolver);
+    private void importNamesFromModule(final List<ImportNormalizer> resolvers,
+                                       final String moduleName,
+                                       final List<String> simpleNames,
+                                       final Map<String, ModuleDeclaration> modules,
+                                       final boolean ignoreCase)
+    {
+        // Give up early if module not found -- will be caught during
+        // validation
+        if (!modules.containsKey(moduleName)) return;
+        final ModuleDeclaration module = modules.get(moduleName);
+        if (module == null) return;
+
+        for (final ModuleExport export : module.getExports()) {
+            if (export.getModulename() != null) {
+                // Consider importing re-exported names from M -- import
+                // intersection of what we want and what is re-exported from M
+                if (export.isStar()) {
+                    for (final ModuleImport imp : module.getImports()) {
+                        if (export.getModulename().equals(imp.getModulename())) {
+                            if (imp.isStar()) {
+                                importNamesFromModule(resolvers,
+                                                      imp.getModulename(),
+                                                      simpleNames,
+                                                      modules, ignoreCase);
+                            } else {
+                                importNamesFromModule(resolvers,
+                                                      imp.getModulename(),
+                                                      intersect_ids(simpleNames, imp.getIdentifiers()),
+                                                      modules, ignoreCase);
+                            }
+                        }
+                    }
+                } else {
+                    final List<String> simpleNames2 = intersect_ids(simpleNames, export.getIdentifiers());
+                    if (!simpleNames.isEmpty()) {
+                        for (final ModuleImport imp : module.getImports()) {
+                            if (export.getModulename().equals(imp.getModulename())) {
+                                if (imp.isStar()) {
+                                    importNamesFromModule(resolvers,
+                                                          imp.getModulename(),
+                                                          simpleNames2,
+                                                          modules,
+                                                          ignoreCase);
+                                } else {
+                                    // Double subtraction:
+                                    // - We wanted A,b,C from M1 == simpleNames
+                                    // - M1 exports A,b from M2  == simpleNames2
+                                    // - M1 imports A from M2
+                                    // (This case should probably be caught by
+                                    // the validator, but let’s be defensive.)
+                                    importNamesFromModule(resolvers,
+                                                          imp.getModulename(),
+                                                          intersect_ids(simpleNames2, imp.getIdentifiers()),
+                                                          modules, ignoreCase);
+                                }
+                            }
+                        }
+                    }
+                }
+            } else {
+                if (export.isStar()) {
+                    // export *;
+                    for (final String id : simpleNames) {
+                        final ImportNormalizer resolver = createImportedNamespaceResolver(moduleName + "." + id, ignoreCase);
+                        if (resolver != null) resolvers.add(resolver);
+                    }
+                } else {
+                    for (final String id : intersect_ids(simpleNames, export.getIdentifiers())) {
+                        final ImportNormalizer resolver = createImportedNamespaceResolver(moduleName + "." + id, ignoreCase);
+                        if (resolver != null) resolvers.add(resolver);
+                    }
+                }
+            }
         }
+    }
+
+    /**
+     * Return a fresh List containing the intersection of the elements of l1
+     * and l2
+     */
+    private List<String> intersect_ids(final List<String> l1, final List<String> l2) {
+        // KLUDGE: this set operation is expensive, but the number of inputs
+        // is small
+        return l1.stream()
+            .filter(s -> l2.contains(s))
+            .collect(Collectors.toList());
     }
 }
