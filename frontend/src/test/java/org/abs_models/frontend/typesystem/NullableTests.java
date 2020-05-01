58b2d606a2fcde7667e62b544956381afa37d2e3
Daniel <d.drodt@gmx.de>
2020-05-01 17:57:39 +0200

    Fix entry/exit

--- frontend/src/test/java/org/abs_models/frontend/typesystem/NullableTests.java
+++ frontend/src/test/java/org/abs_models/frontend/typesystem/NullableTests.java
@@ -9,16 +9,25 @@ import static org.junit.Assert.assertTrue;
 
 import org.abs_models.ABSTest;
 import org.abs_models.frontend.FrontendTest;
-import org.abs_models.frontend.ast.Model;
+import org.abs_models.frontend.ast.*;
+import org.abs_models.frontend.typechecker.KindedName;
 import org.junit.Test;
 
 public class NullableTests extends FrontendTest {
     @Test
     public void test1() {
-        Model m = assertParse("{skip; skip;}");
-        
-        
-        System.out.println(m.getMainBlock().getStmt(0).nullable_out());
+        Model m = assertParse("interface I { Unit m(); } class C implements I { Unit m() { skip; skip; } }");
+        CompilationUnit cu = m.getCompilationUnit(0);
+        ClassDecl d = (ClassDecl) getTestModule(m).lookup(new KindedName(KindedName.Kind.CLASS, "UnitTest.C"));
+        MethodImpl met = d.getMethod(0);
+        Block b = met.getBlock();
+        System.out.println(b.getStmt(0).nullable_out());
         assertTrue(true);
+    }
+
+    static private ModuleDecl getTestModule(Model m) {
+        ModuleDecl md = m.lookupModule("UnitTest");
+        assertNotNull("Module UnitTest not found", md);
+        return md;
     }
 }
