dfa4cf061d5c955a4f2acdd2a235b4ef6bf083e7
Daniel <d.drodt@gmx.de>
2020-05-02 18:01:11 +0200

    Handle calls

--- frontend/src/test/java/org/abs_models/frontend/typesystem/NullableTests.java
+++ frontend/src/test/java/org/abs_models/frontend/typesystem/NullableTests.java
@@ -16,11 +16,13 @@ import org.junit.Test;
 public class NullableTests extends FrontendTest {
     @Test
     public void test1() {
-        Model m = assertParse("interface I { Unit m(); } class C implements I { Unit m() { Object o = this; o = o; } }");
+        Model m = assertParse("interface I { Unit m(I i); } class C implements I { Unit m(I i) { i.m(this); skip; } }");
         CompilationUnit cu = m.getCompilationUnit(0);
         ClassDecl d = (ClassDecl) getTestModule(m).lookup(new KindedName(KindedName.Kind.CLASS, "UnitTest.C"));
         MethodImpl met = d.getMethod(0);
         Block b = met.getBlock();
+        System.out.println(b.getStmt(0).nonNull_in());
+        System.out.println(b.getStmt(0).nonNull_out());
         System.out.println(b.getStmt(1).nonNull_out());
         assertTrue(true);
     }
