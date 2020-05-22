cec6d9e9bbf3030870d64a864f20dfa891fbb346
Daniel <d.drodt@gmx.de>
2020-05-22 18:54:01 +0200

    Simplify implementation

--- frontend/src/test/java/org/abs_models/frontend/typesystem/NullableTests.java
+++ frontend/src/test/java/org/abs_models/frontend/typesystem/NullableTests.java
@@ -1,13 +1,9 @@
 package org.abs_models.frontend.typesystem;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
-import org.abs_models.ABSTest;
 import org.abs_models.frontend.FrontendTest;
 import org.abs_models.frontend.ast.*;
 import org.abs_models.frontend.typechecker.KindedName;
