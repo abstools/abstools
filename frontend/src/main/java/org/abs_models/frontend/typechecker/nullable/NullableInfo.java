dc931d20ab1b039ca701fee8ae49b4ed55177e57
Daniel <d.drodt@gmx.de>
2020-05-22 19:25:22 +0200

    Add NonNull and nullableType attrs

--- frontend/src/main/java/org/abs_models/frontend/typechecker/nullable/NullableInfo.java
+++
@@ -1,5 +0,0 @@
-package org.abs_models.frontend.typechecker.nullable;
-
-public enum NullableInfo {
-  Null, NonNull, Nullable, None,
-}
---
+++ frontend/src/main/java/org/abs_models/frontend/typechecker/nullable/NullableType.java
@@ -0,0 +1,22 @@
+package org.abs_models.frontend.typechecker.nullable;
+
+public enum NullableType {
+  Null, NonNull, Nullable, None;
+
+  public boolean assignableTo(NullableType n) {
+      return NullableType.assignable(n, this);
+  }
+
+  public static boolean assignable(NullableType lhs, NullableType rhs) {
+      if (lhs == NullableType.NonNull) {
+          return rhs == NullableType.NonNull;
+      }
+      if (lhs == NullableType.Nullable) {
+          return true;
+      }
+      if (lhs == NullableType.Null) {
+          return rhs == NullableType.Null;
+      }
+      return true;
+  }
+}
