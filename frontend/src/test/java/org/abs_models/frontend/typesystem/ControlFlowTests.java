fb851e570b1ab54782821b4170ec484faff798bc
Daniel <d.drodt@gmx.de>
2020-05-22 11:19:54 +0200

    Remove unused imports

--- frontend/src/test/java/org/abs_models/frontend/typesystem/ControlFlowTests.java
+++ frontend/src/test/java/org/abs_models/frontend/typesystem/ControlFlowTests.java
@@ -1,17 +1,12 @@
 package org.abs_models.frontend.typesystem;
 
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
-import org.abs_models.ABSTest;
 import org.abs_models.frontend.FrontendTest;
 import org.abs_models.frontend.ast.*;
 import org.abs_models.frontend.typechecker.KindedName;
-import org.abs_models.frontend.typechecker.nullable.SimpleSet;
 import org.junit.Test;
 
 public class ControlFlowTests extends FrontendTest {
