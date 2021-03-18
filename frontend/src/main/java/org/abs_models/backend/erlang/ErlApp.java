abstools/abstools

commit 40fab6d49012828a64570cde7e8452def5ebfe45

Fix some spotbugs code issues
- Handle return values from mkdir()

- Simplify deleting of directories

frontend/src/main/java/org/abs_models/backend/erlang/ErlApp.java
@@ -58,9 +58,10 @@ public class ErlApp {
         this.destDir = destDir;
         this.destCodeDir = new File(destDir, "absmodel/src/");
         this.destIncludeDir = new File(destDir, "absmodel/include/");
-        destDir.mkdirs();
+        if (!destDir.mkdirs() && !destDir.isDirectory()) {
+            throw new IOException("Could not create directory " + destDir.toString());
+        }
         FileUtils.cleanDirectory(destDir);
-        destDir.mkdirs();
         // new File(destDir, "absmodel/ebin").mkdir();
         index_file = http_index_file;
         static_dir = http_static_dir;
@@ -180,7 +181,9 @@ public class ErlApp {
                         String dirname = f.substring(0, f.length() - 2);
                         String inname = JAR_PATH + dirname;
                         String outname = destDir + "/" + dirname;
-                        new File(outname).mkdirs();
+                        if (!(new File(outname).mkdirs())) {
+                            throw new IOException("Could not create directory " + outname);
+                        }
                         copyJarDirectory(((JarURLConnection) resource).getJarFile(),
                                 inname, outname);
                     } else {
@@ -189,7 +192,9 @@ public class ErlApp {
                             throw new RuntimeException("Could not locate Runtime file:" + f);
                         String outputFile = f.replace('/', File.separatorChar);
                         File file = new File(destDir, outputFile);
-                        file.getParentFile().mkdirs();
+                        if (!file.getParentFile().mkdirs()) {
+                            throw new IOException("Could not create directory " + file.getParentFile().toString());
+                        }
                         Files.asByteSink(file).writeFrom(is);
                     }
                 }
@@ -204,12 +209,16 @@ public class ErlApp {
             }
             if (index_file != null) {
                 File http_out_file = new File(destDir, "absmodel/_build/default/lib/absmodel/priv/index.html");
-                http_out_file.getParentFile().mkdirs();
+                if (!http_out_file.getParentFile().mkdirs()) {
+                    throw new IOException("Could not create directory " + http_out_file.getParentFile().toString());
+                }
                 FileUtils.copyFile(index_file, http_out_file);
             }
             if (static_dir != null) {
                 File static_out_dir = new File(destDir, "absmodel/_build/default/lib/absmodel/priv/static/");
-                static_out_dir.mkdirs();
+                if (!static_out_dir.mkdirs()) {
+                    throw new IOException("Could not create directory " + static_out_dir.toString());
+                }
                 FileUtils.copyDirectory(static_dir, static_out_dir);
             }
         } finally {
@@ -231,7 +240,9 @@ public class ErlApp {
                     is = jarFile.getInputStream(entry);
                     ByteStreams.copy(is, Files.asByteSink(new File(outname, relFilename)).openStream());
                 } else {
-                    new File(outname, relFilename).mkdirs();
+                    if (!new File(outname, relFilename).mkdirs()) {
+                        throw new IOException("Could not create directory " + new File(outname, relFilename).toString());
+                    }
                 }
             }
         }

frontend/src/main/java/org/abs_models/backend/java/JavaBackend.java
@@ -89,11 +89,9 @@ public class JavaBackend extends Main {
             printErrorMessage();
             return 1;
         }
-        arguments.destDir.mkdirs();
-        if (!arguments.destDir.exists()) {
-            throw new InternalBackendException("Destination directory " + arguments.destDir.getAbsolutePath() + " does not exist!");
+        if (!arguments.destDir.mkdirs() && !arguments.destDir.isDirectory()) {
+            throw new IOException("Could not create directory " + arguments.destDir.toString());
         }
-
         if (!arguments.destDir.canWrite()) {
             throw new InternalBackendException("Destination directory " + arguments.destDir.getAbsolutePath() + " cannot be written to!");
         }

frontend/src/main/java/org/abs_models/backend/java/codegeneration/JavaCode.java
@@ -9,9 +9,12 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
+import org.apache.commons.io.FileUtils;
 
 
 public class JavaCode {
@@ -20,9 +23,7 @@ public class JavaCode {
     private final List<String> mainClasses = new ArrayList<>();
 
     public JavaCode() throws IOException {
-        srcDir = File.createTempFile("absjavabackend", Long.toString(System.nanoTime()));
-        srcDir.delete();
-        srcDir.mkdir();
+        srcDir = Files.createTempDirectory("absjavabackend").toFile();
     }
 
     public JavaCode(File srcDir) {
@@ -42,7 +43,7 @@ public class JavaCode {
         return res;
     }
 
-    public Package createPackage(String packageName) {
+    public Package createPackage(String packageName) throws IOException {
         return new Package(packageName);
     }
 
@@ -51,11 +52,13 @@ public class JavaCode {
         public final File packageDir;
         private String firstPackagePart;
 
-        public Package(String packageName) {
+        public Package(String packageName) throws IOException {
             this.packageName = packageName;
             this.packageDir = new File(srcDir, packageName.replace('.', File.separatorChar));
             this.firstPackagePart = packageName.split("\\.")[0];
-            packageDir.mkdirs();
+            if (!packageDir.mkdirs() && !packageDir.isDirectory()) {
+                throw new IOException("Could not create directory " + packageDir.toString());
+            }
         }
 
         public File createJavaFile(String name) throws IOException, JavaCodeGenerationException {
@@ -70,7 +73,9 @@ public class JavaCode {
             }
             File file = new File(packageDir, name + ".java");
             addFile(file);
-            file.createNewFile();
+            if (!file.createNewFile()) {
+                throw new IOException("File already exists: " + file.toString());
+            }
             return file;
         }
 
@@ -83,19 +88,8 @@ public class JavaCode {
         return srcDir;
     }
 
-    public void deleteCode() {
-        deleteDir(srcDir);
-    }
-
-    private void deleteDir(File dir) {
-        for (File f : dir.listFiles()) {
-            if (f.isDirectory()) {
-                deleteDir(f);
-            } else {
-                f.delete();
-            }
-        }
-        dir.delete();
+    public void deleteCode() throws IOException {
+        FileUtils.deleteDirectory(srcDir);
     }
 
     public void compile() throws JavaCodeGenerationException {

frontend/src/main/java/org/abs_models/backend/java/codegeneration/JavaGeneratorHelper.java
@@ -47,6 +47,7 @@ import org.abs_models.frontend.ast.VarDecl;
 import org.abs_models.frontend.ast.VarOrFieldDecl;
 import org.abs_models.frontend.ast.VarUse;
 import org.abs_models.frontend.typechecker.Type;
+import org.apache.commons.io.FileUtils;
 
 public class JavaGeneratorHelper {
 
@@ -420,26 +421,11 @@ public class JavaGeneratorHelper {
     /**
      * removes the gen folder and all its contents
      * @param code
+     * @throws IOException
      */
-    public static void cleanGenFolder(JavaCode code) {
+    public static void cleanGenFolder(JavaCode code) throws IOException {
         File genDir = code.getSrcDir();
-        cleanGenFolderRecursively(genDir);
-    }
-
-    private static void cleanGenFolderRecursively(File dir) {
-        if (dir == null) throw new IllegalArgumentException();
-        if (!dir.exists()) return;
-        for (File f : dir.listFiles()) {
-            if (f.isDirectory()) {
-                cleanGenFolderRecursively(f);
-            } else {
-                if (f.getAbsolutePath().endsWith(".java")
-                        || f.getAbsolutePath().endsWith(".class")) {
-                    f.delete();
-                }
-            }
-        }
-        dir.delete();
+        FileUtils.deleteDirectory(genDir);
     }
 
     public static void printEscapedString(PrintStream stream, String content) {

frontend/src/main/java/org/abs_models/backend/java/codegeneration/dynamic/DynamicJavaGeneratorHelper.java
@@ -410,8 +410,9 @@ public class DynamicJavaGeneratorHelper {
     /**
      * removes the gen folder and all its contents
      * @param code
+     * @throws IOException
      */
-    public static void cleanGenFolder(JavaCode code) {
+    public static void cleanGenFolder(JavaCode code) throws IOException {
         JavaGeneratorHelper.cleanGenFolder(code);
     }
 

frontend/src/test/java/org/abs_models/backend/erlang/ErlangTestDriver.java
@@ -90,7 +90,7 @@ public class ErlangTestDriver extends ABSTest implements BackendTestDriver {
     private String runAndCheck(String absCode) throws Exception {
         File f = null;
         try {
-            f = Files.createTempDir();
+            f = java.nio.file.Files.createTempDirectory("erlangtest").toFile();
             f.deleteOnExit();
             Model model = assertParse(absCode, Config.TYPE_CHECK, /* XXX:CI Config.WITH_LOC_INF, */ Config.WITHOUT_MODULE_NAME);
             String mainModule = genCode(model, f, true);

frontend/src/test/java/org/abs_models/backend/java/JavaBackendTest.java
@@ -6,6 +6,7 @@ package org.abs_models.backend.java;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -64,12 +65,12 @@ public class JavaBackendTest extends ABSTest {
         assertValidJava(getJavaCode("module JavaUnitTest; " + absCode, Config.WITHOUT_MODULE_NAME));
     }
 
-    public static void assertValidJava(JavaCode javaCode) throws Exception {
+    public static void assertValidJava(JavaCode javaCode) throws IOException {
         try {
             javaCode.compile("-classpath", LIB_CLASSPATH, "-d", javaCode.getSrcDir().getAbsolutePath()+"/gen/test");
-        } catch (Exception e) {
+        } catch (JavaCodeGenerationException e) {
             System.out.println(javaCode);
-            throw e;
+            fail();
         } finally {
             javaCode.deleteCode();
         }

commit e40e083109e2fe117588299ea369d24bd92b8437

Properly handle return value of `File.mkdirs`
- If the directory already exists, this method will return `false`.

- Fixes the error introduced in commit
  40fab6d49012828a64570cde7e8452def5ebfe45

frontend/src/main/java/org/abs_models/backend/erlang/ErlApp.java
@@ -181,7 +181,8 @@ public class ErlApp {
                         String dirname = f.substring(0, f.length() - 2);
                         String inname = JAR_PATH + dirname;
                         String outname = destDir + "/" + dirname;
-                        if (!(new File(outname).mkdirs())) {
+                        File outdir = new File(outname);
+                        if (! outdir.mkdirs() && !outdir.isDirectory()) {
                             throw new IOException("Could not create directory " + outname);
                         }
                         copyJarDirectory(((JarURLConnection) resource).getJarFile(),
@@ -192,7 +193,7 @@ public class ErlApp {
                             throw new RuntimeException("Could not locate Runtime file:" + f);
                         String outputFile = f.replace('/', File.separatorChar);
                         File file = new File(destDir, outputFile);
-                        if (!file.getParentFile().mkdirs()) {
+                        if (!file.getParentFile().mkdirs() && !file.getParentFile().isDirectory()) {
                             throw new IOException("Could not create directory " + file.getParentFile().toString());
                         }
                         Files.asByteSink(file).writeFrom(is);
@@ -209,14 +210,14 @@ public class ErlApp {
             }
             if (index_file != null) {
                 File http_out_file = new File(destDir, "absmodel/_build/default/lib/absmodel/priv/index.html");
-                if (!http_out_file.getParentFile().mkdirs()) {
+                if (!http_out_file.getParentFile().mkdirs() && !http_out_file.getParentFile().isDirectory()) {
                     throw new IOException("Could not create directory " + http_out_file.getParentFile().toString());
                 }
                 FileUtils.copyFile(index_file, http_out_file);
             }
             if (static_dir != null) {
                 File static_out_dir = new File(destDir, "absmodel/_build/default/lib/absmodel/priv/static/");
-                if (!static_out_dir.mkdirs()) {
+                if (!static_out_dir.mkdirs() && !static_out_dir.isDirectory()) {
                     throw new IOException("Could not create directory " + static_out_dir.toString());
                 }
                 FileUtils.copyDirectory(static_dir, static_out_dir);
@@ -240,7 +241,8 @@ public class ErlApp {
                     is = jarFile.getInputStream(entry);
                     ByteStreams.copy(is, Files.asByteSink(new File(outname, relFilename)).openStream());
                 } else {
-                    if (!new File(outname, relFilename).mkdirs()) {
+                    File newDir = new File(outname, relFilename);
+                    if (!newDir.mkdirs() && !newDir.isDirectory()) {
                         throw new IOException("Could not create directory " + new File(outname, relFilename).toString());
                     }
                 }