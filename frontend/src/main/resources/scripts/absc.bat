9931a4766defebe447a3bc6b6788cd91cbbe8de4
Rudi Schlatte <rudi@ifi.uio.no>
2024-06-26 16:49:18 +0200

    Fix source version of start scripts
    We regenerate the `frontend/bin` directory during build, so re-add this
    to `.gitignore`
    
    This fixes some parts of 61bc89ba6b15fb5a1bd65278ad5021929c751f9d

--- frontend/src/main/resources/scripts/absc.bat
+++ frontend/src/main/resources/scripts/absc.bat
@@ -2,6 +2,9 @@
 
 set BASEDIR=%~dp0..\
 
-java -Xmx512m -jar %BASEDIR%dist\absfrontend.jar %*
+rem This `--add-opens` is necessary until choco-solver is updated; see
+rem https://github.com/abstools/abstools/issues/334
+rem NOTE: untested Cmd syntax below; please report if calling `absc.bat` fails
+java --add-opens java.base/sun.security.action=ALL-UNNAMED -jar %BASEDIR%dist\absfrontend.jar %*
 
 echo on
