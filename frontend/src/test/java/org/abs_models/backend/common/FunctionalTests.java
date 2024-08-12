abstools/abstools

commit 1469a564a7cbddea42135e923058872d2cd3eeff

Refactor algebraic datatype storage
We used to have one member variable per constructor argument, and one
method to access this member.  These methods were never called.

What was used in practice was the method `getArg(i)`, which called
`getArgs` and `getNumArgs`.  `getNumArgs` in turn called `getArgs` to
obtain the number of arguments to the constructor.  `getArgs` created
and returned a fresh array every time it was called ...  Hence,
iterating through the contents of a user-defined datatype, as happens
e.g. during pattern-matching, would create (2x number of constructor
arguments) fresh arrays, each containing all contents.

Instead, store the content array, have `getArgs` return this constant
array, and eliminate the member variables and accessor methods.

frontend/src/main/java/org/abs_models/backend/java/codegeneration/GenerateJava.jadd
@@ -119,17 +119,11 @@ aspect GenerateJava {
 
         stream.println(" {");
 
-        int i = 0;
-        for (ConstructorArg u : getConstructorArgs()) {
-            stream.print("public final ");
-            stream.println(JavaBackend.getJavaType(u) + " arg" + i + ";");
-            stream.println("public " + JavaBackend.getJavaType(u) + " getArg" + i + "() { return arg" + i + "; }");
-            i++;
-        }
+        stream.println("public final " + ABSValue.class.getName() + "[] allArgs;");
 
         stream.print("public " + constructorClassName + "(");
         boolean first = true;
-        i = 0;
+        int i = 0;
         for (ConstructorArg u : getConstructorArgs()) {
             if (first)
                 first = false;
@@ -140,22 +134,18 @@ aspect GenerateJava {
             i++;
         }
         stream.println(") {");
-        i = 0;
-        for (ConstructorArg u : getConstructorArgs()) {
-            stream.println("this.arg" + i + " = arg" + i + ";");
-            i++;
-        }
-        stream.println("}");
-
-        stream.print("protected " + ABSValue.class.getName() + "[] getArgs() { return new " + ABSValue.class.getName() + "[] { ");
+        stream.print("allArgs = new " + ABSValue.class.getName() + "[] { ");
         i=0;
         for (ConstructorArg u : getConstructorArgs()) {
             if (i > 0)
                 stream.print(", ");
-            stream.print("this.arg" + i);
+            stream.print("arg" + i);
             i++;
         }
-        stream.println(" }; }");
+        stream.println(" };");
+        stream.println("}");
+
+        stream.println("protected " + ABSValue.class.getName() + "[] getArgs() { return allArgs; }");
 
         stream.println("public java.lang.String getConstructorName() { return \"" + getName() + "\";}");
 
@@ -163,15 +153,10 @@ aspect GenerateJava {
         stream.println("public " + ABSBool.class.getName() + " eq(" + ABSValue.class.getName() + " o) {");
         stream.println("if (! (o instanceof " + constructorClassName + ")) return " + ABSBool.class.getName() + ".FALSE;");
         stream.println(constructorClassName + " other = (" + constructorClassName + ") o;");
-
-        i = 0;
-        for (ConstructorArg u : getConstructorArgs()) {
-        stream.println("if (!" + BinOp.class.getName() + ".eq(this.arg" + i + ", other.arg" + i + ").toBoolean()) return " + ABSBool.class.getName() + ".FALSE;");
-            i++;
-        }
-
-        stream.println("return " + ABSBool.class.getName() + ".TRUE;");
+        stream.println("boolean allEqual = java.util.stream.IntStream.range(0, allArgs.length).allMatch(i -> " + BinOp.class.getName() + ".eq(allArgs[i], other.allArgs[i]).toBoolean());");
+        stream.println("return "  + ABSBool.class.getName() + ".fromBoolean(allEqual);");
         stream.println("}");
+        stream.println();
 
         // toJSON method
         JavaGeneratorHelper.generateDataTypeConstructorToJsonMethod(stream, this);
@@ -192,17 +177,11 @@ aspect GenerateJava {
 
         stream.println(" {");
 
-        int i = 0;
-        for (ConstructorArg u : getConstructorArgs()) {
-            stream.print("public final ");
-            stream.println(JavaBackend.getJavaType(u) + " arg" + i + ";");
-            stream.println("public " + JavaBackend.getJavaType(u) + " getArg" + i + "() { return arg" + i + "; }");
-            i++;
-        }
+        stream.println("public final " + ABSValue.class.getName() + "[] allArgs;");
 
         stream.print("public " + constructorClassName + "(");
         boolean first = true;
-        i = 0;
+        int i = 0;
         for (ConstructorArg u : getConstructorArgs()) {
             if (first)
                 first = false;
@@ -213,11 +192,15 @@ aspect GenerateJava {
             i++;
         }
         stream.println(") {");
-        i = 0;
+        stream.print("allArgs = new " + ABSValue.class.getName() + "[] { ");
+        i=0;
         for (ConstructorArg u : getConstructorArgs()) {
-            stream.println("this.arg" + i + " = arg" + i + ";");
+            if (i > 0)
+                stream.print(", ");
+            stream.print("arg" + i);
             i++;
         }
+        stream.println(" };");
         stream.println("}");
 
         stream.print("protected " + ABSValue.class.getName() + "[] getArgs() { return new " + ABSValue.class.getName() + "[] { ");
@@ -236,14 +219,8 @@ aspect GenerateJava {
         stream.println("public " + ABSBool.class.getName() + " eq(" + ABSValue.class.getName() + " o) {");
         stream.println("if (! (o instanceof " + constructorClassName + ")) return " + ABSBool.class.getName() + ".FALSE;");
         stream.println(constructorClassName + " other = (" + constructorClassName + ") o;");
-
-        i = 0;
-        for (ConstructorArg u : getConstructorArgs()) {
-        stream.println("if (!" + BinOp.class.getName() + ".eq(this.arg" + i + ", other.arg" + i + ").toBoolean()) return " + ABSBool.class.getName() + ".FALSE;");
-            i++;
-        }
-
-        stream.println("return " + ABSBool.class.getName() + ".TRUE;");
+        stream.println("boolean allEqual = java.util.stream.IntStream.range(0, allArgs.length).allMatch(i -> " + BinOp.class.getName() + ".eq(allArgs[i], other.allArgs[i]).toBoolean());");
+        stream.println("return "  + ABSBool.class.getName() + ".fromBoolean(allEqual);");
         stream.println("}");
 
         // toJSON method

frontend/src/main/java/org/abs_models/backend/java/codegeneration/JavaGeneratorHelper.java
@@ -392,7 +392,7 @@ public class JavaGeneratorHelper {
                 }
                 if (key != null) {
                     stream.println("result.put(\"" + key + "\", "
-                                   + ModelApi.class.getName() + ".absToJson(this.arg" + elem + "));");
+                                   + ModelApi.class.getName() + ".absToJson(this.allArgs[" + elem + "]));");
                 }
             }
             stream.println("return result;");

commit f5bd48996d9d4ee0891bfe1a358489ed3f84b709

Fix exception constructor code generation
Fix a bug introduced in 1469a564a7cbddea42135e923058872d2cd3eeff

frontend/src/main/java/org/abs_models/backend/java/codegeneration/GenerateJava.jadd
@@ -203,15 +203,7 @@ aspect GenerateJava {
         stream.println(" };");
         stream.println("}");
 
-        stream.print("protected " + ABSValue.class.getName() + "[] getArgs() { return new " + ABSValue.class.getName() + "[] { ");
-        i=0;
-        for (ConstructorArg u : getConstructorArgs()) {
-            if (i > 0)
-                stream.print(", ");
-            stream.print("this.arg" + i);
-            i++;
-        }
-        stream.println(" }; }");
+        stream.print("protected " + ABSValue.class.getName() + "[] getArgs() { return allArgs; }");
 
         stream.println("public java.lang.String getConstructorName() { return \"" + getName() + "\";} ");
 

frontend/src/test/java/org/abs_models/backend/common/FunctionalTests.java
@@ -193,7 +193,16 @@ public class FunctionalTests extends SemanticTests {
     
     @Test
     public void exceptionDataType() throws Exception {
-        assertEvalTrue("exception MyE(Bool); { Bool testresult = False; MyE e = MyE(True); switch (e) { MyE(value) => testresult = value; } }");
+        assertEvalTrue("""
+            exception MyE(Bool);
+            {
+                Bool testresult = False;
+                MyE e = MyE(True);
+                switch (e) {
+                    MyE(value) => testresult = value;
+                }
+            }
+            """);
     }
     
     @Test