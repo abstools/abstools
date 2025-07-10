1efbeea894a9e6a2db0363010b83364bbf3c4d6b
Rudi Schlatte <rudi@constantly.at>
2025-07-10 16:03:05 +0200

    Add SPARQL endpoint
    Add an endpoint `/sparql` to the Model API.  This endpoint accepts
    POST requests and returns the result of executing the SPARQL query
    sent in the request body.
    
    TODO:
    - Add a query parameter to choose the output language

--- frontend/src/main/java/org/abs_models/backend/java/observing/GraphObserver.java
+++ frontend/src/main/java/org/abs_models/backend/java/observing/GraphObserver.java
@@ -1,13 +1,16 @@
 package org.abs_models.backend.java.observing;
 
-import java.util.ArrayList;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.WeakHashMap;
+import java.util.logging.Logger;
 
 import org.abs_models.backend.java.lib.runtime.ABSObject;
 import org.abs_models.backend.java.lib.runtime.COG;
+import org.abs_models.backend.java.lib.runtime.Logging;
 import org.abs_models.backend.java.lib.types.ABSAlgebraicDataType;
 import org.abs_models.backend.java.lib.types.ABSInterface;
 import org.apache.jena.query.Query;
@@ -16,10 +19,15 @@ import org.apache.jena.query.QueryExecutionFactory;
 import org.apache.jena.query.QueryFactory;
 import org.apache.jena.query.QuerySolution;
 import org.apache.jena.query.ResultSet;
+import org.apache.jena.query.ResultSetFormatter;
 import org.apache.jena.rdf.model.Model;
 import org.apache.jena.rdf.model.ModelFactory;
 import org.apache.jena.rdf.model.Property;
 import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.riot.Lang;
+import org.apache.jena.sparql.ARQConstants;
+import org.apache.jena.sparql.core.Prologue;
+import org.apache.jena.sparql.resultset.ResultsWriter;
 import org.apfloat.Apint;
 import org.apfloat.Aprational;
 
@@ -28,6 +36,8 @@ import org.apfloat.Aprational;
  * It is used to create an object graph upon request.
  */
 public class GraphObserver extends DefaultSystemObserver implements ObjectCreationObserver {
+
+    protected static final Logger log = Logging.getLogger(GraphObserver.class.getName());
 
     /** The abs language ontology prefix */
     static String absNS = "http://abs-models.org/ns/abs/";
@@ -46,9 +56,9 @@ public class GraphObserver extends DefaultSystemObserver implements ObjectCreati
     private static Set<COGView> cogSet = Collections.newSetFromMap(new WeakHashMap<COGView, Boolean>());
 
     /**
-     * Deactivate garbage collection of registered objects and sets.
-     * Should be done at the end of the model run so the end state is
-     * preserved.
+     * Deactivate garbage collection of registered objects and sets,
+     * and make these sets immmutable.  Should be done at the end of
+     * the model run to preserve the ending state.
      */
     public static synchronized void freezeObserver() {
         objectSet = Set.copyOf(objectSet);
@@ -90,16 +100,32 @@ public class GraphObserver extends DefaultSystemObserver implements ObjectCreati
      * the query string, so do not need to be defined.
      */
     public static List<QuerySolution> runQuery(Model model, String queryString) {
-        List<QuerySolution> result = new ArrayList<>();
         queryString = sparqlPrefix + queryString;
         Query query = QueryFactory.create(queryString);
         try (QueryExecution qexec = QueryExecutionFactory.create(query, model)) {
             ResultSet results = qexec.execSelect();
-            results.forEachRemaining((solution) -> {
-                result.add(solution);
-            });
-            return result;
+            return ResultSetFormatter.toList(results);
         }
+    }
+
+    public static String runQuery(Model model, String queryString, Lang language) {
+        queryString = sparqlPrefix + queryString;
+        String result = "";
+        Query query = QueryFactory.create(queryString);
+        try (QueryExecution qexec = QueryExecutionFactory.create(query, model);
+             ByteArrayOutputStream baos = new ByteArrayOutputStream()) {
+            ResultSet results = qexec.execSelect();
+            ResultsWriter.create()
+                .lang(language)
+                // see ResultSetFormat.out(OutputStream, ResultSet, Prologue)
+                // TODO: figure out why text format uses namespace prefixes but json, xml etc. don't
+                .set(ARQConstants.symPrologue, new Prologue(query.getPrefixMapping()))
+                .write(baos, results);
+            result = baos.toString();
+        } catch (IOException e) {
+            log.warning("Caught exception closing a string output stream; ignoring it since this should not happen.");
+		}
+        return result;
     }
 
     /**
@@ -113,12 +139,12 @@ public class GraphObserver extends DefaultSystemObserver implements ObjectCreati
      * Return an RDF model of the current ABS state.
      */
     public static Model getModel() {
+        Set<ObjectView> objects = Set.copyOf(objectSet);
+        Set<COGView> cogs = Set.copyOf(cogSet);
         Model model = ModelFactory.createDefaultModel();
         model.setNsPrefix("abs", absNS);
         model.setNsPrefix("prog", progNS);
         model.setNsPrefix("run", runNS);
-        Set<ObjectView> objects = Set.copyOf(objectSet);
-        Set<COGView> cogs = Set.copyOf(cogSet);
         for (ObjectView view : objects) {
             addObjectTriples(model, view);
         }
